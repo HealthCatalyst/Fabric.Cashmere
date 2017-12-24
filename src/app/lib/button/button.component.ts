4b61f1387dae299bd00790fa147a5011e1e629a7
Jeremy Trujillo <jeremy.trujillo@healthcatalyst.com>
2017-12-24 02:07:21 -0700

    Finished button unit tests and fixed button demo documentation

--- src/app/lib/button/button.component.ts
+++ src/app/lib/button/button.component.ts
@@ -1,42 +1,48 @@
 /* tslint:disable:component-selector */
+/* tslint:disable:use-host-property-decorator */
 // https://github.com/mgechev/codelyzer/issues/178#issuecomment-265154480
 
-import {Component, ElementRef, Input, OnChanges, Renderer2, SimpleChanges} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
 
 export function throwErrorForInvalidButtonColor() {
-    throw new Error('Unsupported color input value');
+  throw Error('Unsupported color input value');
 }
 
 type ButtonColor = 'primary' | 'primary-alt1' | 'primary-alt2' | 'primary-alt3' | 'secondary' | 'tertiary';
 
 @Component({
-    selector: 'button[hc-button], a[hc-button]',
-    template: '<ng-content></ng-content>',
-    styleUrls: ['./button.component.scss']
+  selector: 'button[hc-button]',
+  template: '<ng-content></ng-content>',
+  styleUrls: ['./button.component.scss'],
+  host: {
+    '[disabled]': 'disabled || null',
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnChanges {
-    private supportedColors = ['primary', 'primary-alt1', 'primary-alt2', 'primary-alt3', 'secondary', 'tertiary'];
+  private supportedColors = ['primary', 'primary-alt1', 'primary-alt2', 'primary-alt3', 'secondary', 'tertiary'];
 
-    @Input() color: ButtonColor = 'primary';
+  @Input() color: ButtonColor = 'primary';
+  @Input() disabled = false;
 
-    constructor(private elementRef: ElementRef,
-                private renderer: Renderer2) {
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer2) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const color = changes['color'];
+    if (color) {
+      if (!this.supportedColors.includes(color.currentValue)) {
+        throwErrorForInvalidButtonColor();
+      }
+      this.changeColor(color.previousValue, color.currentValue);
     }
+  }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        const color = changes['color'];
-        if (color) {
-            if (!this.supportedColors.includes(color.currentValue)) {
-                throwErrorForInvalidButtonColor();
-            }
-            this.changeButtonColor(color.previousValue, color.currentValue);
-        }
+  private changeColor(previousColor, newColor): void {
+    if (previousColor) {
+      this.renderer.removeClass(this.elementRef.nativeElement, `hc-${previousColor}`);
     }
-
-    private changeButtonColor(previousColor, newColor): void {
-        if (previousColor) {
-            this.renderer.removeClass(this.elementRef.nativeElement, previousColor);
-        }
-        this.renderer.addClass(this.elementRef.nativeElement, newColor);
-    }
+    this.renderer.addClass(this.elementRef.nativeElement, `hc-${newColor}`);
+  }
 }
--- src/app/lib/button/button.module.ts
+++ src/app/lib/button/button.module.ts
@@ -1,12 +1,20 @@
 import { ButtonComponent } from './button.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnchorComponent } from './anchor.component';
 
 @NgModule({
   imports: [
-      CommonModule
+    CommonModule
   ],
-  declarations: [ButtonComponent],
-  exports: [ButtonComponent]
+  declarations: [
+    ButtonComponent,
+    AnchorComponent
+  ],
+  exports: [
+    ButtonComponent,
+    AnchorComponent
+  ]
 })
-export class ButtonModule { }
+export class ButtonModule {
+}
