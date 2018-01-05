7ff6be8e4a2562c1ad09f1ec2efdd0bcf56f9b08
Jeremy Trujillo <jeremytru91@gmail.com>
2018-01-05 15:59:33 -0700

    Updated button so it can be applied to a button or anchor and removed… (#6)
    * Updated button so it can be applied to a button or anchor and removed button options
    
    * Updated button unit tests and button demo documentation

---
+++ src/app/lib/button/anchor.component.ts
@@ -0,0 +1,32 @@
+/* tslint:disable:component-selector */
+/* tslint:disable:use-host-property-decorator */
+// https://github.com/mgechev/codelyzer/issues/178#issuecomment-265154480
+
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { ButtonComponent } from './button.component';
+
+@Component({
+  selector: 'a[hc-button]',
+  template: '<ng-content></ng-content>',
+  styleUrls: ['./button.component.scss'],
+  host: {
+    '[attr.tabindex]': 'disabled ? -1 : 0',
+    '[attr.disabled]': 'disabled || null',
+    '[attr.aria-disabled]': 'disabled.toString()'
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AnchorComponent extends ButtonComponent {
+  constructor(elementRef: ElementRef,
+              renderer: Renderer2) {
+    super(elementRef, renderer);
+  }
+
+  @HostListener('click', ['$event'])
+  handleClickEvents(event: Event) {
+    if (this.disabled) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
+}
--- src/app/lib/button/button.component.spec.ts
+++ src/app/lib/button/button.component.spec.ts
@@ -1,101 +1,171 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 
 import { ButtonComponent } from './button.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ButtonOptions } from 'app/lib/cashmere';
+import { ButtonModule } from './button.module';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
-  let button: DebugElement;
-
-  beforeEach(
-    async(() => {
+  beforeEach(async(() => {
       TestBed.configureTestingModule({
-        imports: [],
-        declarations: [ButtonComponent],
-        providers: [],
-        schemas: [NO_ERRORS_SCHEMA]
+        imports: [ButtonModule],
+        declarations: [TestAppComponent]
       })
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.createComponent(ButtonComponent);
-          component = fixture.componentInstance;
-          button = fixture.debugElement.query(By.css('button'));
-
-          const defaultOptions: ButtonOptions = {
-            buttonType: 'primary',
-            buttonDisplayed: true
-          }
-          component.options = defaultOptions;
-
-          fixture.detectChanges();
-        });
+        .compileComponents();
     })
   );
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should apply class based on color property', () => {
+    const testApp = new TestAppReference();
+    const buttonColors = ['primary', 'primary-alt1', 'primary-alt2', 'primary-alt3', 'secondary', 'tertiary'];
+
+    buttonColors.forEach(color => {
+      testApp.setColor(color);
+      testApp.detectChanges();
+      expect(testApp.buttonDebugElement.nativeElement.classList.contains(`hc-${color}`)).toBe(true);
+      expect(testApp.aDebugElement.nativeElement.classList.contains(`hc-${color}`)).toBe(true);
+    });
   });
 
-  it('should call event once when button clicked', () => {
-    spyOn(component, 'clickButton');
-    button.triggerEventHandler('click', null);
-    expect(component.clickButton).toHaveBeenCalledTimes(1);
+  it('should not clear classes already defined on the element', () => {
+    const testApp = new TestAppReference();
+
+    testApp.buttonDebugElement.nativeElement.classList.add('user-class');
+    testApp.aDebugElement.nativeElement.classList.add('user-class');
+    testApp.setColor('primary');
+    testApp.detectChanges();
+
+    expect(testApp.buttonDebugElement.nativeElement.classList.contains('hc-primary')).toBe(true);
+    expect(testApp.buttonDebugElement.nativeElement.classList.contains('user-class')).toBe(true);
+
+    expect(testApp.aDebugElement.nativeElement.classList.contains('hc-primary')).toBe(true);
+    expect(testApp.aDebugElement.nativeElement.classList.contains('user-class')).toBe(true);
+
+    testApp.setColor('secondary');
+    testApp.detectChanges();
+
+    expect(testApp.buttonDebugElement.nativeElement.classList.contains('hc-secondary')).toBe(true);
+    expect(testApp.buttonDebugElement.nativeElement.classList.contains('user-class')).toBe(true);
+
+    expect(testApp.aDebugElement.nativeElement.classList.contains('hc-secondary')).toBe(true);
+    expect(testApp.aDebugElement.nativeElement.classList.contains('user-class')).toBe(true);
   });
 
-  it('should emit click once when button clicked', () => {
-    spyOn(component.onButtonClick, 'emit');
-    button.triggerEventHandler('click', null);
-    expect(component.onButtonClick.emit).toHaveBeenCalledTimes(1);
+  it('should throw an error when unsupported color is used', () => {
+    const testApp = new TestAppReference();
+
+    expect(() => {
+      testApp.setColor('bigBirdYellow');
+      testApp.detectChanges()
+    }).toThrow();
   });
 
-  it('should not emit click when nothing happens', () => {
-    spyOn(component.onButtonClick, 'emit');
-    expect(component.onButtonClick.emit).not.toHaveBeenCalled();
+  describe('button[hc-button]', () => {
+    it('should set disabled property on host button', () => {
+      const testApp = new TestAppReference();
+
+      expect(testApp.buttonDebugElement.nativeElement.disabled).toBeFalsy('Expected button not to be disabled');
+
+      testApp.setIsDisabled(true);
+      testApp.detectChanges();
+
+      expect(testApp.buttonDebugElement.nativeElement.disabled).toBeTruthy('Expected button to be disabled');
+    });
+
+    it('should increment when disabled is false', () => {
+      const testApp = new TestAppReference();
+
+      testApp.setIsDisabled(false);
+      testApp.detectChanges();
+
+      testApp.buttonDebugElement.nativeElement.click();
+      expect(testApp.testAppComponent.clickCount).toEqual(1);
+    });
+
+    it('should not increment when button is disabled', () => {
+      const testApp = new TestAppReference();
+
+      testApp.setIsDisabled(true);
+      testApp.detectChanges();
+
+      testApp.buttonDebugElement.nativeElement.click();
+      expect(testApp.testAppComponent.clickCount).toEqual(0);
+    });
   });
 
-  it('primary button should have styles set properly', () => {
-    component.options = {
-      buttonType: 'primary'
-    }
-    fixture.detectChanges();
-    expect(button.classes['btn']).toBe(true);
-    expect(button.classes['btn-primary']).toBe(true);
+  describe('a[hc-button]', () => {
+    it('should not redirect if disabled', () => {
+      const testApp = new TestAppReference();
+      testApp.setIsDisabled(true);
+      testApp.detectChanges();
+
+      testApp.aDebugElement.nativeElement.click();
+    });
+
+    it('should add aria-disabled when disabled', () => {
+      const testApp = new TestAppReference();
+      testApp.detectChanges();
+      expect(testApp.aDebugElement.nativeElement.getAttribute('aria-disabled')).toBe('false');
+      expect(testApp.aDebugElement.nativeElement.getAttribute('disabled')).toBeNull();
+
+      testApp.setIsDisabled(true);
+      testApp.detectChanges();
+      expect(testApp.aDebugElement.nativeElement.getAttribute('aria-disabled')).toBe('true');
+      expect(testApp.aDebugElement.nativeElement.getAttribute('disabled')).not.toBeNull();
+    });
+
+    it('should not add aria-disabled when disabled is false', () => {
+      const testApp = new TestAppReference();
+      testApp.detectChanges();
+      expect(testApp.aDebugElement.nativeElement.getAttribute('aria-disabled')).toBe('false');
+      expect(testApp.aDebugElement.nativeElement.getAttribute('disabled')).toBeNull();
+
+      testApp.setIsDisabled(false);
+      testApp.detectChanges();
+      expect(testApp.aDebugElement.nativeElement.getAttribute('aria-disabled')).toBe('false');
+      expect(testApp.aDebugElement.nativeElement.getAttribute('disabled')).toBeNull();
+    });
   });
-
-  it('primary button should not have btn-tertiary style', () => {
-    component.options = {
-      buttonType: 'primary'
-    }
-    fixture.detectChanges();
-    expect(button.classes['btn']).toBe(true);
-    expect(button.classes['btn-tertiary']).toBeFalsy();
-  });
-
-  it('tertiary button should not have btn-primary style', () => {
-    component.options = {
-      buttonType: 'tertiary'
-    }
-    fixture.detectChanges();
-    expect(button.classes['btn']).toBe(true);
-    expect(button.classes['btn-primary']).toBeFalsy();
-  });
-
-  it('icon style should be settable', () => {
-    const iconOptions: ButtonOptions = {
-      buttonType: 'primary',
-      buttonDisplayed: true,
-      buttonIcon: 'fa fa-rocket'
-    }
-    component.options = iconOptions;
-
-    fixture.detectChanges();
-    let icon: DebugElement = fixture.debugElement.query(By.css('span'));
-    // change updates nativeElement but not debugElement
-    expect(icon.nativeElement.className).toBe('fa fa-rocket');
-  });
-
 });
+
+class TestAppReference {
+  fixture: ComponentFixture<TestAppComponent>;
+  testAppComponent: TestAppComponent;
+  buttonDebugElement: DebugElement;
+  aDebugElement: DebugElement;
+
+  constructor() {
+    this.fixture = TestBed.createComponent(TestAppComponent);
+    this.testAppComponent = this.fixture.debugElement.componentInstance;
+    this.buttonDebugElement = this.fixture.debugElement.query(By.css('button'));
+    this.aDebugElement = this.fixture.debugElement.query(By.css('a'));
+  }
+
+  setColor(color: string): void {
+    this.testAppComponent.buttonColor = color;
+  }
+
+  setIsDisabled(isDisabled: boolean): void {
+    this.testAppComponent.isDisabled = isDisabled;
+  }
+
+  detectChanges() {
+    this.fixture.detectChanges();
+  }
+}
+
+@Component({
+  template: `
+      <button hc-button [color]="buttonColor" [disabled]="isDisabled" (click)="buttonClick()">Button</button>
+      <a hc-button [color]="buttonColor" [disabled]="isDisabled" href="https://www.healthcatalyst.com">Link</a>
+  `
+})
+class TestAppComponent {
+  buttonColor = 'primary';
+  isDisabled = false;
+  clickCount = 0;
+
+  buttonClick() {
+    this.clickCount++;
+  }
+}
