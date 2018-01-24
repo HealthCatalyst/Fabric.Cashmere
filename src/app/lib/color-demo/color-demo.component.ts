358a5abc844aa8a040c92334e26866449567da9c
Cory Fugate <cory.fugate@healthcatalyst.com>
2018-01-24 14:14:06 -0700

    minor cleanup to color demo

--- src/app/lib/color-demo/color-demo.component.ts
+++ src/app/lib/color-demo/color-demo.component.ts
@@ -5,10 +5,4 @@ import { Component, ViewEncapsulation } from '@angular/core';
     templateUrl: './color-demo.component.html',
     styleUrls: ['./color-demo.component.scss']
 })
-export class ColorDemoComponent {
-    showTemplate: boolean = true;
-
-    viewToggle(show: 'ts' | 'html') {
-        this.showTemplate = show === 'html';
-    }
-}
+export class ColorDemoComponent {}
