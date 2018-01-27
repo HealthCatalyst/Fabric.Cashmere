2b68aeb379bb68f577319a93b82f05a7d7b6febc
Jeremy Trujillo <jeremy.trujillo@cmcflex.com>
2018-01-27 14:12:49 -0700

    Added icon component
    Closes #41

--- src/app/demo/demo-routes.ts
+++ src/app/demo/demo-routes.ts
@@ -4,33 +4,38 @@ import { SelectDemoComponent } from 'app/lib/select/select-demo/select-demo.comp
 import { DemoComponent } from 'app/demo/demo.component';
 import { ButtonDemoComponent } from 'app/lib/button/button-demo/button-demo.component';
 import { CheckboxDemoComponent } from 'app/lib/checkbox/checkbox-demo/checkbox-demo.component';
+import { IconDemoComponent } from 'app/lib/icon/icon-demo/icon-demo.component';
 import { VerticalTabsDemoComponent } from 'app/lib/vertical-tabs/vertical-tabs-demo/vertical-tabs-demo.component';
 
 export const routes: Routes = [
-    {
-        path: 'demo',
-        component: DemoComponent,
-        children: [
-            {
-                path: 'color',
-                component: ColorDemoComponent
-            },
-            {
-                path: 'button',
-                component: ButtonDemoComponent
-            },
-            {
-                path: 'select',
-                component: SelectDemoComponent
-            },
-            {
-                path: 'checkbox',
-                component: CheckboxDemoComponent
-            },
-            {
-                path: 'verticaltabs',
-                component: VerticalTabsDemoComponent
-            },
-        ]
-    }
+  {
+    path: 'demo',
+    component: DemoComponent,
+    children: [
+      {
+        path: 'color',
+        component: ColorDemoComponent
+      },
+      {
+        path: 'button',
+        component: ButtonDemoComponent
+      },
+      {
+        path: 'select',
+        component: SelectDemoComponent
+      },
+      {
+        path: 'checkbox',
+        component: CheckboxDemoComponent
+      },
+      {
+        path: 'icon',
+        component: IconDemoComponent
+      },
+      {
+        path: 'verticaltabs',
+        component: VerticalTabsDemoComponent
+      }
+    ]
+  }
 ];
