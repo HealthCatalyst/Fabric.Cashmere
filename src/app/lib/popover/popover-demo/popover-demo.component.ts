c949e7669834f7f3178eeeebd0e3194ac16c1f17
Ben Anderson <ben.anderson@healthcatalyst.com>
2018-01-30 13:57:00 -0700

    navbar and popover documentation

--- src/app/lib/popover/popover-demo/popover-demo.component.ts
+++ src/app/lib/popover/popover-demo/popover-demo.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopoverDemoComponent implements OnInit {
   public body: string = 'dynamic content';
+  public popoverWithListItems: string =
+`<button hc-button color="primary" [hcPopover]="options" popperPlacement="bottom" aria-hidden="true">
+  <i class="fa fa-gear" aria-hidden="true"></i>
+  &nbsp;Options&nbsp;
+  <i class="fa fa-angle-down" aria-hidden="true"></i>
+</button>
+<hc-popover-content #options>
+  <ul class="list-options">
+    <li>
+      <a href="https://www.healthcatalyst.com/" target="_blank">Health Catalyst</a>
+    </li>
+    <li>
+      <a href="https://community.healthcatalyst.com/" target="_blank">Health Catalyst Community</a>
+    </li>
+    <li>
+      <button (click)="aboutClick($event)">About</button>
+    </li>
+  </ul>
+</hc-popover-content>`;
   constructor() { }
 
   ngOnInit() {
