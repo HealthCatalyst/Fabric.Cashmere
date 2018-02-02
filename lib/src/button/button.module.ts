5828248131a190c1af0706ae400e2355e06a10b8
Joe Skeen <joe.skeen@healthcatalyst.com>
2018-02-02 16:43:35 -0700

    code review changes

--- lib/src/button/button.module.ts
+++ lib/src/button/button.module.ts
@@ -1,7 +1,7 @@
 import { ButtonComponent } from './button.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { AnchorComponent } from './anchor.component';
+import { AnchorComponent } from './anchor.component';
 
 @NgModule({
     imports: [
@@ -9,11 +9,11 @@ import { CommonModule } from '@angular/common';
     ],
     declarations: [
         ButtonComponent,
-        // AnchorComponent
+        AnchorComponent
     ],
     exports: [
         ButtonComponent,
-        // AnchorComponent
+        AnchorComponent
     ]
 })
 export class ButtonModule {
