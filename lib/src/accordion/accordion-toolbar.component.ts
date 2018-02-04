7c123dd24469da3ebae2724fde2f0ce451bfed3c
Jeremy Trujillo <jeremy.trujillo@healthcatalyst.com>
2018-02-04 14:13:47 -0700

    Adds Accordion component
    Closes #99

---
+++ lib/src/accordion/accordion-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'hc-accordion-toolbar',
+    template: '<ng-content></ng-content>',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AccordionToolbarComponent {
+    @HostBinding('class.hc-accordion-toolbar') true;
+}
