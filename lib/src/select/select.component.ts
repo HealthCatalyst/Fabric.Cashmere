8624c68fc2a558c69a06dff886c1164653d937e3
Ty Jones <whotyjones@gmail.com>
2018-02-05 19:24:27 -0700

    Toggle dropdown arrow

--- lib/src/select/select.component.ts
+++ lib/src/select/select.component.ts
@@ -2,59 +2,67 @@ import { Component, Input, OnInit, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-    selector: 'hc-select',
-    templateUrl: './select.component.html',
-    styleUrls: ['./select.component.scss'],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => SelectComponent),
-            multi: true
-        }
-    ]
+  selector: 'hc-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
 })
 export class SelectComponent implements ControlValueAccessor {
 
-    @Input() placeholder: string;
-    @Input() options: Array<string> = [];
-    @Input() disabled: boolean = false;
-    disabledAlpha = 0.4;
-    enabledAlpha = 1.0;
-    _value: string;
-    onChange: any = () => {
-    };
-    onTouched: any = () => {
-    };
+  @Input() placeholder: string;
+  @Input() options: Array<string> = [];
+  @Input() disabled: boolean = false;
+  closed: boolean = true;
+  disabledAlpha = 0.4;
+  enabledAlpha = 1.0;
+  _value: string;
+  onChange: any = () => { };
+  onTouched: any = () => { };
 
-    get alpha() {
-        return this.disabled ? this.disabledAlpha : this.enabledAlpha;
-    }
+  get alpha() {
+    return this.disabled ? this.disabledAlpha : this.enabledAlpha;
+  }
 
-    get value() {
-        return this._value;
-    }
+  get value() {
+    return this._value;
+  }
 
-    set value(val: string) {
-        this._value = val;
-        this.onChange(val);
-        this.onTouched();
-    }
+  set value(val: string) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouched();
+  }
 
-    constructor() {
-    }
+  constructor() {}
 
-    registerOnChange(fn: any) {
-        this.onChange = fn;
-    }
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
 
-    registerOnTouched(fn: any) {
-        this.onTouched = fn;
-    }
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
 
-    writeValue(value: string) {
-        if (value) {
-            this._value = value;
-        }
+  writeValue(value: string) {
+    if (value) {
+      this._value = value;
     }
+  }
+
+  toggleArrow(element: Element) {
+      this.closed = !this.closed;
+      (element.parentElement as Element).className = this.closed ? 'closed' : 'open';
+  }
+
+  resetArrow(element: Element) {
+      this.closed = true;
+      (element.parentElement as Element).className = 'closed';
+  }
 
 }
