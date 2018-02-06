609bffbce2d4ed61b9c6ae0ac66e705f56a9d010
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-02-05 21:31:25 -0700

    Hamburger Menu

--- lib/src/navbar/navbar.module.ts
+++ lib/src/navbar/navbar.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { NavbarLinkComponent } from './navbar-link/navbar-link.component';
 import { NavbarIconComponent } from './navbar-icon/navbar-icon.component';
+import { IconModule } from '../icon';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        IconModule
     ],
     declarations: [NavbarComponent, NavbarLinkComponent, NavbarIconComponent],
     exports: [NavbarComponent, NavbarLinkComponent, NavbarIconComponent]
