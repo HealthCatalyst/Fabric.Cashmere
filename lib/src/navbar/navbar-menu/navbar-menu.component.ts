b1d8e216d31c6fcb09f57b112c8b6b1fb88d625b
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-02-07 14:25:18 -0700

    Responsive Menu First Pass

--- lib/src/navbar/navbar-menu/navbar-menu.component.ts
+++ lib/src/navbar/navbar-menu/navbar-menu.component.ts
@@ -1,17 +1,54 @@
-import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { IconModule } from '../../icon/icon.module';
+import { IAppSwitcherService, IDiscoveryApplication } from '../../app-switcher/app-switcher-interfaces';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
     selector: 'hc-navbar-menu',
     templateUrl: './navbar-menu.component.html',
-    styleUrls: ['./navbar-menu.component.scss']
+    styleUrls: ['./navbar-menu.component.scss'],
+    animations: [
+        trigger('slideInOut', [
+            state('in', style({
+                transform: 'translate3d(0, -100%, 0)'
+            })),
+            state('out', style({
+                transform: 'translate3d(0, 0, 0)'
+            })),
+            transition('in => out', animate('400ms ease-in-out')),
+            transition('out => in', animate('400ms ease-in-out'))
+        ]),
+      ]
 })
 export class NavbarMenuComponent implements OnInit {
-    @Input() active?: boolean;
 
-    constructor() {
-    }
+    public applications: IDiscoveryApplication[];
+    public subscription: Subscription;
+
+    private ngUnsubscribe: any = new Subject();
+
+    menuState:string = 'in';
+
+    @Input() appSwitcher: boolean = true;
+
+    constructor( @Inject('IAppSwitcherService') public appSwitcherService: IAppSwitcherService ) { }
 
     ngOnInit() {
+        this.subscription = this.appSwitcherService.getApplications()
+            .takeUntil(this.ngUnsubscribe)
+            .subscribe((response: any) => {
+                this.applications = response.value;
+            });
     }
 
+    ngOnDestroy() {
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
+    }
+
+    show(): void { this.menuState = 'out'; }
+
+    hide(): void { this.menuState = 'in'; }
 }
--- lib/src/navbar/navbar.component.ts
+++ lib/src/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, QueryList, ContentChildren } from '@angular/core';
+import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 
 @Component({
     selector: 'hc-navbar',
@@ -15,10 +16,38 @@ export class NavbarComponent implements OnInit {
 
     @Input() fixedTop: boolean = false;
 
-    constructor() {
-    }
+    menuOpen: boolean = false;
+    hamburger: string = 'inline';
+    hamburgerClose: string = 'none';
+
+    @ContentChildren(NavbarMenuComponent) mobileMenu: QueryList <NavbarMenuComponent>;
 
     ngOnInit() {
     }
 
+    toggleMobileMenu() {
+        if( this.mobileMenu.first ) {
+            if( this.menuOpen ) {
+                this.mobileMenu.first.hide();
+                this.menuOpen = false;
+                this.hamburger = 'inline';
+                this.hamburgerClose = 'none';
+            }
+            else {
+                this.mobileMenu.first.show();
+                this.menuOpen = true;
+                this.hamburger = 'none';
+                this.hamburgerClose = 'inline';
+            }
+        }
+    }
+
+    menuClick( event ) {
+        let clickTarget: string = event.target.classList[0];
+
+        //Verify that the click in the mobile menu came from a navigation item
+        if( clickTarget.indexOf("menu-dropdown") == -1 )
+            this.toggleMobileMenu();
+
+    }
 }
