c48b259ac10f30ddcbae3b3914ea622f66e41ed0
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-02-23 14:36:58 -0700

    Cory's Feedback

--- lib/src/chip/chip-row/chip-row.component.ts
+++ lib/src/chip/chip-row/chip-row.component.ts
@@ -4,26 +4,18 @@ import { anyToBoolean } from '../../util';
 
 @Component({
     selector: 'hc-chip-row',
-    template: `<div [ngClass]="contentClass"><div><div class="row-buffer"><ng-content></ng-content></div></div></div>`,
+    template:   `<div class="chip-row-contents" [ngClass]="{'single-row': !_wrap}"><div>
+                    <div class="row-buffer"><ng-content></ng-content></div>
+                </div></div>`,
     styleUrls: ['../chip.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class ChipRowComponent {
     _wrap: boolean = true;
-    contentClass: string = 'chip-row-contents';
 
-    constructor( ) {
-    }
+    constructor( ) {}
 
-    @Input()
-    get wrap(): boolean {
-        return this._wrap;
-    }
+    @Input() get wrap(): boolean { return this._wrap; }
 
-    set wrap(doWrap) {
-        this._wrap = anyToBoolean(doWrap);
-        if ( !this._wrap ) {
-            this.contentClass = 'chip-row-contents single-row';
-        }
-    }
+    set wrap(doWrap) { this._wrap = anyToBoolean(doWrap); }
 }
--- lib/src/chip/chip.component.ts
+++ lib/src/chip/chip.component.ts
@@ -1,38 +1,21 @@
-import { Component, Input, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { anyToBoolean } from '../util';
 
 @Component({
     selector: 'hc-chip',
-    template: `<div [ngClass]="chipType"><ng-content></ng-content></div>`,
+    template: `<div [class]="color" [ngClass]="{'close': _action, 'chip': true}"><ng-content></ng-content></div>`,
     styleUrls: ['./chip.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 
-export class ChipComponent implements OnInit {
+export class ChipComponent {
 
-    _action: boolean = false;
-    @Input() color: string = 'neutral';
-    chipType: string = 'chip';
+    @Input() _action: boolean = false;
+    @Input() color: 'neutral' | 'yellow' | 'green' | 'red' = 'neutral';
 
     constructor() {}
 
-    ngOnInit() {
-        this.chipType += ' ' + this.color;
-    }
+    @Input() get action(): boolean { return this._action; }
 
-    @Input()
-    get action(): boolean {
-        return this._action;
-    }
-
-    set action(isAction) {
-        this._action = anyToBoolean(isAction);
-        if ( this._action ) {
-            this.chipType = 'chip close';
-        } else {
-            this.chipType = 'chip';
-        }
-
-        this.chipType += ' ' + this.color;
-    }
+    set action(isAction) { this._action = anyToBoolean(isAction); }
 }
