f5f3048c46c74f066e951d5b2a2ff6aeb085f6f7
Cory Tak <cory.tak@healthcatalyst.com>
2018-03-05 13:28:38 -0700

    Enable overlay clicks above and below

--- lib/src/modal/modal-window.component.ts
+++ lib/src/modal/modal-window.component.ts
@@ -33,7 +33,27 @@ export class ModalWindowComponent {
 
      @HostListener('click', ['$event'])
      overlayClick(event: any) {
-         if (!this.ignoreOverlayClick && this.el.nativeElement === event.target) {
+         let modalContentNotPresent = true;
+         let modalWindowTargetIncluded = event.path.findIndex(p => p === this.el.nativeElement) > -1;
+         let classList: (DOMTokenList | undefined)[] = event.path.map(p => p.classList);
+         for (let cl of classList) {
+             if (cl) {
+                 if (cl.contains('hc-modal-content')) {
+                    modalContentNotPresent = false;
+                 }
+             }
+         }
+
+         /* The hc-modal has 100% height (although not completely visible).
+            To enable closing the modal while clicking above or below the modal (in
+            addition to the sides) this function will check to see if the click
+            event includes:
+                1. This window element
+                2. Not the hc-modal-content and
+                3. the overlay click option is disabled. */
+         if (!this.ignoreOverlayClick
+             && modalContentNotPresent
+             && modalWindowTargetIncluded) {
              this.activeModal.close();
          }
      }
