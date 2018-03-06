1d5154a5fa662b5ac25ca21ba8bfc5ee0906bd1e
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-03-06 14:26:18 -0700

    Code Styles Section

--- docs/app/styles/styles.module.ts
+++ docs/app/styles/styles.module.ts
@@ -13,6 +13,8 @@ import { TableDemoComponent } from './table/table-demo.component';
 import { SwatchDemoComponent } from './color/swatch-demo.component';
 import { routes } from './styles-routes';
 import { TypographyDemoComponent } from './typography/typography-demo.component';
+import { CodeDemoComponent } from './code/code-demo.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     imports: [
@@ -22,6 +24,7 @@ import { TypographyDemoComponent } from './typography/typography-demo.component'
         SubnavModule,
         TileModule,
         SelectModule,
+        SharedModule,
         RouterModule.forRoot(routes)
     ],
     declarations: [
@@ -29,7 +32,8 @@ import { TypographyDemoComponent } from './typography/typography-demo.component'
         ColorDemoComponent,
         TableDemoComponent,
         SwatchDemoComponent,
-        TypographyDemoComponent
+        TypographyDemoComponent,
+        CodeDemoComponent
     ]
 })
 export class StylesModule {
---
+++ guides/styles/code.md
@@ -0,0 +1,20 @@
+:::
+##### Code Blocks
+For blocks of code larger than than three lines, it is recommended that line numbers be included.  The line numbers should be the same font size as the code, but in a light gray color so they aren't distracting.
+
+``` html
+    <hc-list>
+        <hc-list-item>
+            <hc-icon fontSet="fa" fontIcon="fa-snowflake-o" hcListIcon></hc-icon>
+            <h4 hcListLine>SnowFlake</h4>
+            <span hcListLine>Second Line</span>
+        </hc-list-item>
+    </hc-list>
+```
+:::
+
+:::
+##### Syntax Highlighting
+Syntax highlighting can be useful to make blocks of code more readable.  The highlight.js library is our recommended highligher, using the **"GitHub Gist"** theme.  Visit [https://highlightjs.org/static/demo/](https://highlightjs.org/static/demo/) for more details.
+
+:::
