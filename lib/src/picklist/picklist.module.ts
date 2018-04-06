06b28de78856f84d8c891f9fd82ca841b1ad3a21
HQCATALYST\cory.fugate <cory.fugate@healthcatalyst.com>
2018-04-06 16:05:03 -0600

    use hc-input in picklist component

--- lib/src/picklist/picklist.module.ts
+++ lib/src/picklist/picklist.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PicklistPaneComponent } from './pane/picklist-pane.component';
 import { PicklistComponent } from './picklist.component';
 import { ButtonModule } from '../button/button.module';
+import { IconModule } from '../icon/icon.module';
+import { InputModule } from '../input/input.module';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ButtonModule],
+    imports: [CommonModule, FormsModule, ButtonModule, IconModule, InputModule],
     declarations: [
         PicklistPaneComponent,
         PicklistComponent
