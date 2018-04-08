a185ec6d831c71c180bb9afaa568a0bf2a60b4b0
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-04-07 21:13:09 -0600

    Collapsing Menu

--- lib/src/navbar/navbar-link/navbar-link.component.ts
+++ lib/src/navbar/navbar-link/navbar-link.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, ElementRef, ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'hc-navbar-link',
     templateUrl: './navbar-link.component.html'
 })
-export class NavbarLinkComponent implements OnInit {
+export class NavbarLinkComponent {
     @HostBinding('class') hostClass = 'navbar-item';
     @Input() active?: boolean;
     @Input() uri: string;
     @Input() linkText: string;
     @Input() exact: boolean = false;
+    hidden: boolean = false;
 
-    constructor() {
+    constructor( private el:ElementRef, private ref: ChangeDetectorRef ) {
     }
 
-    ngOnInit() {
-    }
+    hide() { this.hidden = true; this.ref.detectChanges(); }
+    show() { this.hidden = false; this.ref.detectChanges(); }
 
+    getWidth() { return this.el.nativeElement.scrollWidth; }
 }
--- lib/src/navbar/navbar.component.ts
+++ lib/src/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input, QueryList, ContentChildren } from '@angular/core';
+import { Component, Input, QueryList, ContentChildren, ElementRef, HostListener, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { NavbarMobileMenuComponent } from './navbar-mobile-menu/navbar-mobile-menu.component';
+import { NavbarLinkComponent } from './navbar-link/navbar-link.component';
 
 @Component({
     selector: 'hc-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements AfterViewInit {
 
     @Input() user: string = '';
 
@@ -19,13 +20,68 @@ export class NavbarComponent implements OnInit {
     @Input() fixedTop: boolean = false;
 
     @ContentChildren(NavbarMobileMenuComponent) mobileMenu: QueryList <NavbarMobileMenuComponent>;
+    @ContentChildren(NavbarLinkComponent) navLinks: QueryList <NavbarLinkComponent>;
+
+    linkWidths: Array<number> = [];
+    linksMax = 0;
+    collapse: boolean = false;
+
+    @HostListener('window:resize') navResize()
+    {
+        //If links is zero the page is smaller than the first responsive breakpoint
+        if ( this.el.nativeElement.querySelector('.links').clientWidth > 0 )
+        {
+            let navbarWidth: number = this.el.nativeElement.querySelector('.navbar').scrollWidth;
+            let logo: number = this.el.nativeElement.querySelector('.navbar-app').scrollWidth;
+            let icons: number = this.el.nativeElement.querySelector('.icon').scrollWidth;
+            let more: number = 116;
+            let switcher: number = 55;
+            let links: number = this.linksMax;
+
+            console.log( navbarWidth + ", " + switcher + ", " + logo + ", " + links + ", " + icons );
+
+            if ( navbarWidth <= (switcher + logo + links + icons) )
+            {
+                this.collapse = true;
+                let tempArray = this.navLinks.toArray();
+                tempArray.reverse();
+
+                tempArray[0].hide();
+                links -= this.linkWidths[0];
+
+                for ( let i = 1; i < tempArray.length; i++ )
+                {
+                    if ( navbarWidth <= (switcher + logo + links + more + icons) ) {
+                        tempArray[i].hide();
+                        links -= this.linkWidths[i];
+                    }
+                    else { tempArray[i].show(); }
+                }
+            }
+            else {
+                this.collapse = false;
+                this.navLinks.forEach(t => (t.show()));
+            }
+            this.ref.detectChanges();
+        }
+    }
 
     menuOpen: boolean = false;
 
-    constructor() {
+    constructor( private el:ElementRef, private ref: ChangeDetectorRef ) {
     }
 
-    ngOnInit() {
+    ngAfterViewInit() {
+        let scope = this;
+        setTimeout( function() {
+            scope.navLinks.forEach(t => {
+                scope.linksMax += t.getWidth();
+                scope.linkWidths.push( t.getWidth() );
+            } );
+            scope.linkWidths.reverse();
+
+            scope.navResize()
+        }, 100 );
     }
 
     toggleMobileMenu() {
