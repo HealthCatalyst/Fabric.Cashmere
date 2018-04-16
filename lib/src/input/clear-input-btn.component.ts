57cb88adad8e0b61022033869b73c784ffaae0de
HQCATALYST\cory.fugate <cory.fugate@healthcatalyst.com>
2018-04-16 17:10:35 -0600

    Avoid memory leak from clear search button by cleaning up the event listener.

--- lib/src/input/clear-input-btn.component.ts
+++ lib/src/input/clear-input-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { InputComponent } from './input.component';
 @Component({
     selector: 'hc-clear-input-btn',
@@ -6,17 +6,21 @@ import { InputComponent } from './input.component';
     `<hc-icon *ngIf="inputEl?.value.length > 0" icon-sm fontSet="fa" fontIcon="fa-times-circle"
     class="hc-clear-btn" title="Clear" (click)="clear()"></hc-icon>`
 })
-export class ClearInputBtnComponent {
+export class ClearInputBtnComponent implements OnDestroy{
     @Input() public set input(input: HTMLInputElement | InputComponent) { this.setInput(input); }
     public inputEl?: HTMLInputElement;
+    private checkShouldShowClearBtn = () => { this.cd.detectChanges(); };
 
     constructor(private cd: ChangeDetectorRef) {}
 
+    public ngOnDestroy() { this.removeEventListenerIfNeeded(); }
+
     public setInput(input: HTMLInputElement | InputComponent) {
+        this.removeEventListenerIfNeeded();
         this.inputEl = this.isComponent(input) ? input.elementRef.nativeElement : this.input;
 
         if (this.inputEl) {
-            this.inputEl.addEventListener('input', () => { this.cd.detectChanges(); });
+            this.inputEl.addEventListener('input', this.checkShouldShowClearBtn);
         }
     }
 
@@ -34,5 +38,11 @@ export class ClearInputBtnComponent {
     private isComponent(input: HTMLInputElement | InputComponent): input is InputComponent {
         const component = (<InputComponent>input);
         return component && !!component.elementRef;
+    }
+
+    private removeEventListenerIfNeeded() {
+        if (this.inputEl) {
+            this.inputEl.addEventListener('input', this.checkShouldShowClearBtn);
+        }
     }
 }
