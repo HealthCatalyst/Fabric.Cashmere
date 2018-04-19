0167f25e5553d48ae11a41d1178089733bdb0b81
ckeates <cdkeates@gmail.com>
2018-04-19 10:29:35 -0600

    Enhancement: Radio button Angular forms support (#213)
    * refactor radio button into group directive and single button component; add notifier service to allow for singular selections based on radio button name;
    
    * more linting fixes; make radio group directive optional and use forwardRef in case init order changes; remove dependency on notifier service;
    
    * disable circular deps warning until forwardRef is recognized; move directive into its own component to satisfy nglint;
    
    * update radio button demo and documentation;
    
    * add basic unit testing for radio group directive;
    
    * documentation cleanup;
    
    * flip circular deps warning back to true;
    
    * change anyToBoolean to new parseBooleanAttribute method;

--- lib/src/radio-button/radio-button.module.ts
+++ lib/src/radio-button/radio-button.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RadioButtonComponent } from './radio-button.component';
-
+import { RadioGroupDirective } from './radio-group.directive';
 @NgModule({
     imports: [
         CommonModule
     ],
     exports: [
-        RadioButtonComponent
+        RadioButtonComponent,
+        RadioGroupDirective
     ],
     declarations: [
-        RadioButtonComponent
-    ]
+        RadioButtonComponent,
+        RadioGroupDirective
+    ],
+    providers: []
 })
 export class RadioButtonModule {
 }
