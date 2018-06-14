3924fd1a3f36fda5ae747583df24e12054714e86
Jeremy Trujillo <jeremytru91@gmail.com>
2018-06-13 18:49:30 -0600

    fix: fix replace-package-version.sh (#322)
    fix sed for linux by removing empty quotes(only needed for osx)

--- scripts/replace-package-version.sh
+++ scripts/replace-package-version.sh
@@ -9,7 +9,7 @@ fi
 PACKAGE_VERSION=$1
 
 # Replace package.json version with generated version
-sed -i "" -E "s/(\"version\":[[:space:]]*\").+(\")/\1${PACKAGE_VERSION}\2/g" package.json
+sed -i -E "s/(\"version\":[[:space:]]*\").+(\")/\1${PACKAGE_VERSION}\2/g" package.json
 
 # Update package-lock.json version also
-sed -i "" -E "s/(\"version\":[[:space:]]*\").+(\")/\1${PACKAGE_VERSION}\2/g" package-lock.json
+sed -i -E "s/(\"version\":[[:space:]]*\").+(\")/\1${PACKAGE_VERSION}\2/g" package-lock.json
