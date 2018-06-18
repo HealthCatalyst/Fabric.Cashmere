b6deb3483b0a9119446316e3355fd000c9c21674
Jeremy Trujillo <jeremytru91@gmail.com>
2018-06-18 16:27:15 -0600

    Drawer update (#332)
    * feat(drawer component): added side mode to drawer component
    
    drawer component can now have a drawer and its content sit next to each other without pushing the
    content out of frame. fixed issue where drawer and drawer content animation were out-of-sync
    
    * refactor(drawer component): drawer component styling updates
    
    drawer component now uses ViewEncapsulation.none to allow greater dev flexibility. drawer now
    correctly handles changes to its size through async actions.

--- projects/cashmere/src/lib/drawer/drawer.component.ts
+++ projects/cashmere/src/lib/drawer/drawer.component.ts
@@ -7,10 +7,13 @@ import {
     HostBinding,
     HostListener,
     Input,
-    Output
+    Output,
+    ViewEncapsulation
 } from '@angular/core';
 import {animate, AnimationEvent, state, style, transition, trigger} from '@angular/animations';
 import {parseBooleanAttribute} from '../util';
+import {filter, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 export class DrawerPromiseResult {
     constructor(public type: 'open' | 'close') {}
@@ -20,37 +23,59 @@ export class DrawerPromiseResult {
     selector: 'hc-drawer',
     template: '<ng-content></ng-content>',
     styleUrls: ['drawer.component.scss'],
+    encapsulation: ViewEncapsulation.None,
     animations: [
         trigger('openState', [
             state(
                 'open, open-instant',
                 style({
-                    transform: 'translate3d(0, 0, 0)'
+                    transform: 'translate3d(0, 0, 0)',
+                    visibility: 'visible'
                 })
             ),
             state(
                 'void',
                 style({
+                    'box-shadow': 'none',
                     visibility: 'hidden'
                 })
             ),
             transition('void => open-instant', animate('0ms')),
-            transition('void <=> open', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
+            transition('void <=> open, open-instant => void', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
         ])
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DrawerComponent implements AfterContentInit {
-    @Input() mode: 'over' | 'push' = 'push';
+    readonly _openChange = new EventEmitter<boolean>();
+
+    @Input() mode: 'over' | 'push' | 'side' = 'push';
     @Input() align: 'left' | 'right' = 'left';
 
-    @Output() openStart = new EventEmitter();
-    @Output() closeStart = new EventEmitter();
-    @Output() open = new EventEmitter();
-    @Output() close = new EventEmitter();
+    @Output()
+    get openStart(): Observable<void> {
+        return this._animationStarted.pipe(filter(event => event.fromState === 'void' && event.toState === 'open'), map(() => {}));
+    }
+
+    @Output()
+    get closeStart(): Observable<void> {
+        return this._animationStarted.pipe(filter(event => event.fromState === 'open' && event.toState === 'void'), map(() => {}));
+    }
+
+    @Output('opened')
+    get _openStream() {
+        return this._openChange.pipe(filter(value => value), map(() => {}));
+    }
+
+    @Output('closed')
+    get _closeStream() {
+        return this._openChange.pipe(filter(value => !value), map(() => {}));
+    }
 
     @HostBinding() tabindex = -1;
-    @HostBinding('class.drawer') drawerClass = true;
+    @HostBinding('class.hc-drawer') drawerClass = true;
+
+    readonly _animationStarted = new EventEmitter<AnimationEvent>();
 
     private animationDisabled = true;
     private drawerOpened = false;
@@ -70,27 +95,27 @@ export class DrawerComponent implements AfterContentInit {
         return this.elementRef.nativeElement.offsetWidth;
     }
 
-    @HostBinding('class.drawer-opened')
+    @HostBinding('class.hc-drawer-opened')
     get isOpened() {
         return this.drawerOpened && !this.animationPromise;
     }
 
-    @HostBinding('class.drawer-opening')
+    @HostBinding('class.hc-drawer-opening')
     get isOpening() {
         return this.drawerOpened && !!this.animationPromise;
     }
 
-    @HostBinding('class.drawer-closed')
+    @HostBinding('class.hc-drawer-closed')
     get isClosed() {
         return !this.drawerOpened && !this.animationPromise;
     }
 
-    @HostBinding('class.drawer-closing')
+    @HostBinding('class.hc-drawer-closing')
     get isClosing() {
         return !this.drawerOpened && !!this.animationPromise;
     }
 
-    @HostBinding('class.drawer-right')
+    @HostBinding('class.hc-drawer-right')
     get isRight() {
         return this.align === 'right';
     }
@@ -105,22 +130,12 @@ export class DrawerComponent implements AfterContentInit {
 
     @HostListener('@openState.start', ['$event'])
     onAnimationStart(event: AnimationEvent) {
-        const {fromState, toState} = event;
-
-        if (fromState === 'void' && toState === 'open') {
-            this.openStart.emit();
-        } else if (fromState === 'open' && toState === 'void') {
-            this.closeStart.emit();
-        }
+        this._animationStarted.emit(event);
     }
 
     @HostListener('@openState.done', ['$event'])
     onAnimationEnd(event: AnimationEvent) {
-        if (this.opened) {
-            this.open.emit();
-        } else {
-            this.close.emit();
-        }
+        this._openChange.next(this.opened);
 
         if (this.animationPromise) {
             this.resolveAnimationPromise();
