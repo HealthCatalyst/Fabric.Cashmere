d15a6ca7103ce519647964740bd07c97cd2b1303
Jeremy Trujillo <jeremytru91@gmail.com>
2018-06-20 11:59:57 -0600

    docs: fix docs import path
    Fixes dgeni generated import statement for each component. Adds Google's copyright notice to files copied from material 2

--- tools/dgeni/index.ts
+++ tools/dgeni/index.ts
@@ -1,3 +1,27 @@
+/*
+* The MIT License
+*
+* Copyright (c) 2018 Google LLC.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+ */
+
 import {Package} from 'dgeni';
 import {patchLogService} from './patch-log-service';
 import {DocsPrivateFilter} from './processors/docs-private-filter';
@@ -93,7 +117,7 @@ apiDocsPackage.config((readTypeScriptModules: ReadTypeScriptModules, tsParser: T
     const typescriptPathMap: any = {};
 
     materialPackages.forEach(packageName => {
-        typescriptPathMap[`@healthcatalyst/cashmere/${packageName}`] = [`./lib/${packageName}/index.ts`];
+        typescriptPathMap[`@healthcatalyst/cashmere`] = [`./lib/${packageName}/index.ts`];
     });
 
     // Add proper path mappings to the TSParser service of Dgeni. This ensures that properties
