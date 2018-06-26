4f0d5ce7313aae3f566c374071e81f62b5e5469e
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-06-26 15:16:03 -0600

    docs: lint line length correction

--- projects/cashmere/src/lib/tile/tile.component.ts
+++ projects/cashmere/src/lib/tile/tile.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
-/** Container element to help segment content visually against a gray background. The tile will expand to the height and width of the content it contains. */
+/** Container element to help segment content visually against a gray background.
+ * The tile will expand to the height and width of the content it contains. */
 @Component({
     selector: 'hc-tile',
     template: `<ng-content></ng-content>`,
