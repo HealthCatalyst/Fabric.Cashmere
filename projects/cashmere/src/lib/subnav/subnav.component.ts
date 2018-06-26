b2e3f88b4c5113fc188cadb97eb45a66a408e8f2
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-06-26 13:57:30 -0600

    docs: add jsdocs for subnav & tile components
    add comments and underscores to comply with jsdocs
    
    closes #310 & #312

--- projects/cashmere/src/lib/subnav/subnav.component.ts
+++ projects/cashmere/src/lib/subnav/subnav.component.ts
@@ -1,14 +1,16 @@
 import {Component, HostBinding, Input} from '@angular/core';
 
+/** Secondary navigation bar appearing below the primary navbar  */
 @Component({
     selector: 'hc-subnav',
     template: `<ng-content></ng-content>`
 })
 export class SubnavComponent {
+    @HostBinding('class.subnav') _hostClass: boolean = true;
+
     @HostBinding('class.fixed-top')
-    @Input()
-    fixedTop: boolean = false;
-    @HostBinding('class.subnav') hostClass: boolean = true;
+    /** If true, statically positions the subnav below the navbar (stays pinned on scroll) */
+    @Input() public fixedTop: boolean = false;
 
     constructor() {}
 }
--- projects/cashmere/src/lib/tile/tile.component.ts
+++ projects/cashmere/src/lib/tile/tile.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 
+/** Container element to help segment content visually against a gray background. The tile will expand to the height and width of the content it contains. */
 @Component({
     selector: 'hc-tile',
     template: `<ng-content></ng-content>`,
