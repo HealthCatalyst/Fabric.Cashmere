d6e700b5d7604d50aa2e820dbd1a64746f4bab51
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-06-29 13:29:11 -0600

    docs: add jsdocs for chips (#359)
    document chip's public api while hiding fields/methods that are private
    
    closes #297

--- projects/cashmere/src/lib/chip/chip-row/chip-row.component.ts
+++ projects/cashmere/src/lib/chip/chip-row/chip-row.component.ts
@@ -1,19 +1,21 @@
 import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {parseBooleanAttribute} from '../../util';
 
+/** Supporting component to help with grouping chips into collections */
 @Component({
     selector: 'hc-chip-row',
-    template: `<div class="chip-row-contents" [ngClass]="{'single-row': !_wrap}"><div>
+    template: `<div class="chip-row-contents" [ngClass]="{'single-row': !wrap}"><div>
                     <div class="row-buffer"><ng-content></ng-content></div>
                 </div></div>`,
     styleUrls: ['../chip.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class ChipRowComponent {
-    _wrap: boolean = true;
+    private _wrap: boolean = true;
 
     constructor() {}
 
+    /** If false, constrain the container to one line with overflow ellipses (default=true) */
     @Input()
     get wrap(): boolean {
         return this._wrap;
