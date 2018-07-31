8f69a094707f5351e873a6da7c078fee30b555d6
HQCATALYST\cory.fugate <cory.fugate@healthcatalyst.com>
2018-07-31 11:57:49 -0600

    docs(picklist): picklist example update
    trying to get prettier to stop complaining

--- projects/cashmere-examples/src/lib/picklist-valueset/picklist-valueset-example.ts
+++ projects/cashmere-examples/src/lib/picklist-valueset/picklist-valueset-example.ts
@@ -8,24 +8,26 @@ import {Component} from '@angular/core';
     templateUrl: 'picklist-valueset-example.html'
 })
 export class PicklistValuesetExample {
-    public myModel: { values: null, valueset: null};
+    public myModel: {values: null; valueset: null};
 
     public config = {
         codeIsSignificant: true,
         useValuesets: true,
         options: {
-            values: [{code: '001', title: 'Cholera'},
-            {code: '001.0', title: 'Cholera due to vibrio cholerae'},
-            {code: '001.1', title: 'Cholera due to vibrio cholerae el tor'},
-            {code: '001.9', title: 'Cholera, unspecified'},
-            {code: '002', title: 'Typhoid and paratyphoid fevers'},
-            {code: '002.0', title: 'Typhoid fever'},
-            {code: '002.1', title: 'Paratyphoid fever A'},
-            {code: '002.2', title: 'Paratyphoid fever B'},
-            {code: '002.3', title: 'Paratyphoid fever C'},
-            {code: '002.9', title: 'Paratyphoid fever, unspecified'},
-            {code: '003', title: 'Other salmonella infections'},
-            {code: '003.0', title: 'Salmonella gastroenteritis'}],
+            values: [
+                {code: '001', title: 'Cholera'},
+                {code: '001.0', title: 'Cholera due to vibrio cholerae'},
+                {code: '001.1', title: 'Cholera due to vibrio cholerae el tor'},
+                {code: '001.9', title: 'Cholera, unspecified'},
+                {code: '002', title: 'Typhoid and paratyphoid fevers'},
+                {code: '002.0', title: 'Typhoid fever'},
+                {code: '002.1', title: 'Paratyphoid fever A'},
+                {code: '002.2', title: 'Paratyphoid fever B'},
+                {code: '002.3', title: 'Paratyphoid fever C'},
+                {code: '002.9', title: 'Paratyphoid fever, unspecified'},
+                {code: '003', title: 'Other salmonella infections'},
+                {code: '003.0', title: 'Salmonella gastroenteritis'}
+            ],
             valueSets: [
                 {
                     title: 'Cholera',
@@ -46,9 +48,9 @@ export class PicklistValuesetExample {
                         {code: '002.0', title: 'Typhoid fever'},
                         {code: '002.1', title: 'Paratyphoid fever A'},
                         {code: '002.2', title: 'Paratyphoid fever B'},
-                        {code: '002.3', title: 'Paratyphoid fever C'},
+                        {code: '002.3', title: 'Paratyphoid fever C'}
                     ]
-                },
+                }
             ]
         }
     };
