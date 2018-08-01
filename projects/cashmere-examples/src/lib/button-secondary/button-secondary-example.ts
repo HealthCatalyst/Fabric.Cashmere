5c06e5c98bfc5b294ab8d1f11f6b6f9ce4c689a2
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-08-01 14:07:22 -0600

    docs(button): split up examples (#371)
    * docs(button): split up examples
    breaks up one large example into several smaller for buttons

---
+++ projects/cashmere-examples/src/lib/button-secondary/button-secondary-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Secondary Buttons
+ */
+@Component({
+    selector: 'button-secondary-example',
+    templateUrl: 'button-secondary-example.html',
+    styles: ['button { margin: 40px 10px 10px 10px; }']
+})
+export class ButtonSecondaryExample {}
