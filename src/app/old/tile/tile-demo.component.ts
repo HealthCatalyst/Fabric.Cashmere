4fa6648fedecd15c34002d79ffa12b7543b36a4c
Jeremy Trujillo <jeremy.trujillo@healthcatalyst.com>
2018-08-10 10:53:11 -0600

    refactor: removed old commented-out demos
    close #435

--- src/app/old/tile/tile-demo.component.ts
+++
@@ -1,9 +0,0 @@
-// import {Component} from '@angular/core';
-//
-// @Component({
-//     selector: 'hc-tile-demo',
-//     templateUrl: './tile-demo.component.html'
-// })
-// export class TileDemoComponent {
-//     lastModified: Date = new Date(document.lastModified);
-// }
