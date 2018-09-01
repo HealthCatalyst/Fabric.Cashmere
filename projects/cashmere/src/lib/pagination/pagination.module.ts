0a6e964e7d90a84e732e9b0288bea5a38c64cdea
Cory Fugate <cory.fugate@gmail.com>
2018-08-31 20:17:02 -0600

    feat(pagination): add page size
    Updates button hc-secondary styling. Reduces hc-select padding-right. Updates pagination-overview
    example with new pagination page size example.
    
    BREAKING CHANGE: replaced totalPages input with length input
    
    closes #451, fixes #445, fixes #316, fixes #478

--- projects/cashmere/src/lib/pagination/pagination.module.ts
+++ projects/cashmere/src/lib/pagination/pagination.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ButtonModule} from '../button/button.module';
+import {SelectModule} from '../select/select.module';
 import {IconModule} from '../icon/icon.module';
 import {PaginationComponent} from './pagination.component';
 
 @NgModule({
-    imports: [CommonModule, ButtonModule, IconModule],
+    imports: [CommonModule, ButtonModule, IconModule, SelectModule],
     declarations: [PaginationComponent],
-    exports: [PaginationComponent],
-    providers: []
+    exports: [PaginationComponent]
 })
 export class PaginationModule {}
