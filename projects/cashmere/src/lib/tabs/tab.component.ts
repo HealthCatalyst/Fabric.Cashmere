9d9f8d2a0174995bb92033eae593bef7158f8810
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-10-26 10:38:06 -0600

    feat(tabs): support HTML titles
    allows tab titles to include HTML markup instead of only plain text
    
    closes #259

--- projects/cashmere/src/lib/tabs/tab.component.ts
+++ projects/cashmere/src/lib/tabs/tab.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
     template: `<div [hidden]="!_active">
@@ -8,11 +9,17 @@ import {Component, Input} from '@angular/core';
     styles: []
 })
 export class TabComponent {
-    /** Text to be displayed on the tab */
+    /** Title of the tab (HTML markup supported) */
     @Input() tabTitle: string = '';
     /** Router path that the tab routes to. If one tab uses the routerLink in a tab set, all must use the router link.
      * Can be specified as '/path/2' or ['path', '2']
      */
     @Input() routerLink: any[] | string;
     _active: boolean = false;
+
+    constructor(private sanitizer: DomSanitizer) {}
+
+    _safeHtmlTitle(): SafeHtml {
+        return this.sanitizer.bypassSecurityTrustHtml(this.tabTitle);
+    }
 }
