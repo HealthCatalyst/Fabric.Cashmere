72727675a7a7eade10ce70c53bc3386398d94eed
Jacquie Howard <jacquie.howard@healthcatalyst.com>
2018-11-14 11:58:03 -0700

    Added unit tests for util.ts and fixed issues arising

---
+++ projects/cashmere/src/lib/util.spec.ts
@@ -0,0 +1,52 @@
+import { parseBooleanAttribute } from "./util";
+
+describe('parseBooleanAttribute', () => {
+    describe('when value is boolean true', () => {
+        it('should return boolean true', () => {
+            const result = parseBooleanAttribute (true);
+            expect (result).toBe(true);
+        });
+    });
+    describe('when value is boolean false', () => {
+        it('should return boolean false', () => {
+            const result = parseBooleanAttribute (false);
+            expect (result).toBe(false);
+        });
+    });
+    describe('when value is string true', () => {
+        it('should return boolean true', () => {
+            const result = parseBooleanAttribute ('true');
+            expect (result).toBe(true);
+        });
+    });
+    describe('when value is string TRUE', () => {
+        it('should return boolean true', () => {
+            const result = parseBooleanAttribute ('TRUE');
+            expect (result).toBe(true);
+        });
+    });
+    describe('when value is string false', () => {
+        it('should return boolean false', () => {
+            const result = parseBooleanAttribute ('false');
+            expect (result).toBe(false);
+        });
+    });
+    describe('when value is string FALSE', () => {
+        it('should return boolean false', () => {
+            const result = parseBooleanAttribute ('FALSE');
+            expect (result).toBe(false);
+        });
+    });
+    describe('when value is empty string', () => {
+        it('should return boolean true', () => {
+            const result = parseBooleanAttribute ('');
+            expect (result).toBe(true);
+        });
+    });
+    describe('when value is any other value', () => {
+        it('should throw an error', () => {
+            const wrap =  () => parseBooleanAttribute ('1234');
+            expect (wrap).toThrowError('1234 is not a boolean value');
+        });
+    });
+});
