222c81ba1b9a78b26c6455c805e9c98f2b4bfc2e
HQCATALYST\cory.fugate <cory.fugate@healthcatalyst.com>
2018-12-12 14:56:14 -0700

    docs(table sort demo): revert change

--- projects/cashmere-examples/src/lib/table-sort/table-sort-example.ts
+++ projects/cashmere-examples/src/lib/table-sort/table-sort-example.ts
@@ -9,17 +9,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'B', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'A', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'D', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'C', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'a', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'c', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'd', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'b', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: '1', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: '2', weight: 20.1797, symbol: 'Ne'},
-    {position: 11, name: '', weight: 2.4232, symbol: ''}
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'}
 ];
 
 /**
