b18e195f1c78b2ee2164d0f04a8be85577ab0933
HQCATALYST\cory.fugate <cory.fugate@healthcatalyst.com>
2019-01-28 16:27:55 -0700

    test(table-data-source): fix tests

--- projects/cashmere/src/lib/table/table-data-source.spec.ts
+++ projects/cashmere/src/lib/table/table-data-source.spec.ts
@@ -7,30 +7,29 @@ interface PeriodicElement {
     id: number;
     weight: number;
     discoveryDate: Date;
-    sortTestCol: any;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-    {id: 1, name: 'Hydrogen', weight: 1.0079, sortTestCol: 1, discoveryDate: new Date('January 2 1900')},
-    {id: 2, name: 'Helium', weight: 4.0026, sortTestCol: 12341234, discoveryDate: new Date('January 1 1900')},
-    {id: 3, name: 'Lithium', weight: 6.941, sortTestCol: 'C', discoveryDate: new Date('January 1 1800')},
-    {id: 4, name: 'Beryllium', weight: 9.0122, sortTestCol: 'A', discoveryDate: new Date('January 1 2000')},
-    {id: 5, name: 'Boron', weight: 10.811, sortTestCol: 'a', discoveryDate: new Date('January 1 1956')},
-    {id: 6, name: 'Carbon', weight: 12.0107, sortTestCol: 'c', discoveryDate: new Date('January 1 1945')},
-    {id: 7, name: 'Nitrogen', weight: 14.0067, sortTestCol: '1A', discoveryDate: new Date('January 1 1976')},
-    {id: 8, name: 'Oxygen', weight: 15.9994, sortTestCol: '1B', discoveryDate: new Date('January 1 1923')},
-    {id: 9, name: 'Fluorine', weight: 18.9984, sortTestCol: '2C', discoveryDate: new Date('February 1 1900')},
-    {id: 10, name: 'Neon', weight: 20.1797, sortTestCol: 'B', discoveryDate: new Date('May 1 1900')},
-    {id: 11, name: '1CasHydrogen', weight: 10.0079, sortTestCol: 12341234, discoveryDate: new Date('November 1 1900')},
-    {id: 12, name: 'CasHelium', weight: 40.0026, sortTestCol: 2, discoveryDate: new Date('June 1 1900')},
-    {id: 13, name: '2CasLithium', weight: 60.941, sortTestCol: 3, discoveryDate: new Date('January 1 1010')},
-    {id: 14, name: 'CasBeryllium', weight: 90.0122, sortTestCol: null, discoveryDate: new Date('January 1 1923')},
-    {id: 15, name: '3CasBoron', weight: 100.811, sortTestCol: '', discoveryDate: new Date('January 1 2001')},
-    {id: 16, name: 'CasCarbon', weight: 120.0107, sortTestCol: undefined, discoveryDate: new Date('January 1 2002')},
-    {id: 17, name: 'CasNitrogen', weight: 140.0067, sortTestCol: NaN, discoveryDate: new Date('January 1 2004')},
-    {id: 18, name: 'CasOxygen', weight: 150.9994, sortTestCol: new Date('January 1 1990'), discoveryDate: new Date('January 1 2005')},
-    {id: 19, name: '', weight: 180.9984, sortTestCol: new Date('January 1 1970'), discoveryDate: new Date('January 1 2007')},
-    {id: 20, name: '7CasNeon', weight: 200.1797, sortTestCol: new Date('January 1 1980'), discoveryDate: new Date('January 1 2006')}
+    {id: 1, name: 'Hydrogen', weight: 1.0079, discoveryDate: new Date('January 2 1900')},
+    {id: 2, name: 'Helium', weight: 4.0026, discoveryDate: new Date('January 1 1900')},
+    {id: 3, name: 'Lithium', weight: 6.941, discoveryDate: new Date('January 1 1800')},
+    {id: 4, name: 'Beryllium', weight: 9.0122, discoveryDate: new Date('January 1 2000')},
+    {id: 5, name: 'Boron', weight: 10.811, discoveryDate: new Date('January 1 1956')},
+    {id: 6, name: 'Carbon', weight: 12.0107, discoveryDate: new Date('January 1 1945')},
+    {id: 7, name: 'Nitrogen', weight: 14.0067, discoveryDate: new Date('January 1 1976')},
+    {id: 8, name: 'Oxygen', weight: 15.9994, discoveryDate: new Date('January 1 1923')},
+    {id: 9, name: 'Fluorine', weight: 18.9984, discoveryDate: new Date('February 1 1900')},
+    {id: 10, name: 'Neon', weight: 20.1797, discoveryDate: new Date('May 1 1900')},
+    {id: 11, name: '1CasHydrogen', weight: 10.0079, discoveryDate: new Date('November 1 1900')},
+    {id: 12, name: 'CasHelium', weight: 40.0026, discoveryDate: new Date('June 1 1900')},
+    {id: 13, name: '2CasLithium', weight: 60.941, discoveryDate: new Date('January 1 1010')},
+    {id: 14, name: 'CasBeryllium', weight: 90.0122, discoveryDate: new Date('January 1 1923')},
+    {id: 15, name: '3CasBoron', weight: 100.811, discoveryDate: new Date('January 1 2001')},
+    {id: 16, name: 'CasCarbon', weight: 120.0107, discoveryDate: new Date('January 1 2002')},
+    {id: 17, name: 'CasNitrogen', weight: 140.0067, discoveryDate: new Date('January 1 2004')},
+    {id: 18, name: 'CasOxygen', weight: 150.9994, discoveryDate: new Date('January 1 2005')},
+    {id: 19, name: '', weight: 180.9984, discoveryDate: new Date('January 1 2007')},
+    {id: 20, name: '7CasNeon', weight: 200.1797, discoveryDate: new Date('January 1 2006')}
 ];
 
 describe('HcTableDataSource', () => {
@@ -52,35 +51,6 @@ describe('HcTableDataSource', () => {
     });
 
     describe('sortData()', () => {
-        it('sorts mixed values', () => {
-            const sort = new HcSort();
-            sort.direction = 'asc';
-            sort.active = 'sortTestCol';
-            const result = dataSource.sortData(ELEMENT_DATA, sort);
-            expect(result.map(e => e.sortTestCol).toString()).toBe(
-                [
-                    '',
-                    '',
-                    '',
-                    1,
-                    12341234,
-                    '1A',
-                    '1B',
-                    '2C',
-                    'A',
-                    'a',
-                    'B',
-                    'C',
-                    'c',
-                    2,
-                    3,
-                    12341234,
-                    NaN,
-                    new Date('January 1 1970'),
-                    new Date('January 1 1980'),
-                    new Date('January 1 1990')
-            ].toString());
-        });
         it('sorts numbers',
          () => {
             const sort = new HcSort();
