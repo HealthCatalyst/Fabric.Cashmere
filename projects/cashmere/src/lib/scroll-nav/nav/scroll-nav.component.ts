9aae870870dd8811b4051f0d88a63d8c0e4a0880
Cory Fugate <cory.fugate@healthcatalyst.com>
2019-05-28 15:17:12 -0600

    feat(scroll-nav): new public function & bugfixes
    reset and scroll function for scroll-nav content area and fixes for auto height and a fix for
    hcScrollLink navigation

--- projects/cashmere/src/lib/scroll-nav/nav/scroll-nav.component.ts
+++ projects/cashmere/src/lib/scroll-nav/nav/scroll-nav.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewEncapsulation, Renderer2, AfterViewInit, QueryList, ContentChildren} from '@angular/core';
+import {Component, ElementRef, ViewEncapsulation, AfterViewInit, QueryList, ContentChildren} from '@angular/core';
 import { ScrollNavLinkDirective } from './scroll-nav-link.directive';
 
 /** Container for scroll navigation links. */
@@ -13,25 +13,22 @@ export class HcScrollNavComponent implements AfterViewInit {
     public get _links(): Array<HTMLElement> { return this.linkList.toArray().map(e => e._el.nativeElement); }
     private readonly ACTIVE_CLASS = 'hc-scroll-nav-active';
 
-    constructor(public _elementRef: ElementRef, private renderer: Renderer2) {}
+    constructor(public _elementRef: ElementRef) {}
 
     public ngAfterViewInit(): void {
-        this._links.forEach((e, i) => {
+        this.linkList.forEach((e, i) => {
             if (i === 0) {
-                e.classList.add(this.ACTIVE_CLASS);
+                e._el.nativeElement.classList.add(this.ACTIVE_CLASS);
             }
-            this.renderer.listen(e, 'click', () => {
-                this.onNavClick(e);
-            });
         });
     }
 
     public _setActiveClassById(id: string): void {
-        const link = this._links.find(e => e.getAttribute('hcScrollLink') === id);
+        const link = this.linkList.find(e => e.hcScrollLink === id);
         if (!link) {
             throw new Error(`Failed to mark active class. Could not find the element with the data target for id: ${id}.`);
         }
-        this.setActiveClass(link);
+        this.setActiveClass(link._el.nativeElement);
     }
 
     private setActiveClass(element: HTMLElement): void {
@@ -39,19 +36,5 @@ export class HcScrollNavComponent implements AfterViewInit {
             e.classList.remove(this.ACTIVE_CLASS);
         });
         element.classList.add(this.ACTIVE_CLASS);
-    }
-
-    private onNavClick(element: HTMLElement): void {
-        const id = element.getAttribute('hcScrollLink') || '';
-        this.navigateToSection(id);
-    }
-
-    private navigateToSection(id: string) {
-        const el = document.getElementById(id);
-        if (!el) {
-            throw new Error(`Failed to navigate. Could not find the element with the id: ${id}.`);
-        } else {
-            el.scrollIntoView();
-        }
     }
 }
