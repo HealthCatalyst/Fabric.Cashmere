8f7f6a4ac305b7dc3f8ec4991e17e52aa032ec9e
Travis Foxley <tfoxley@wcf.com>
2019-08-19 10:36:14 -0600

    fix(progressbar): change focusing and completing functionality
    BREAKING CHANGE: progress items now use an enum for status

--- projects/cashmere/src/lib/progress-bar/index.ts
+++ projects/cashmere/src/lib/progress-bar/index.ts
@@ -1,3 +1,4 @@
 export {ProgressBarComponent} from './progress-bar.component';
 export {ProgressBarModule} from './progress-bar.module';
 export {ProgressItem} from './progress-item.interface';
+export {ProgressItemStatus} from './progress-item-status';
--- projects/cashmere/src/lib/progress-bar/progress-bar.component.spec.ts
+++ projects/cashmere/src/lib/progress-bar/progress-bar.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ProgressBarComponent} from './progress-bar.component';
+import {ProgressItemStatus} from '@wcf-insurance/cashmere';
 
 describe('ProgressBarComponent', () => {
     let component: ProgressBarComponent;
@@ -47,60 +48,39 @@ describe('ProgressBarComponent', () => {
             expect(component.items.length).toEqual(1);
         });
 
-        it('should set greatestCompletedItemIndex when given a list of at least one progress item', () => {
+        it('should select a progress item when when given a list of at least one progress item', () => {
             // arrange
+            const aCompletedItem = {id: 'test', title: 'test', status: ProgressItemStatus.COMPLETE};
+            let unSelectedItem = {id: 'test2', title: 'test2'};
+            let expectedSelectedItem = {id: 'test3', title: 'test3', focused: true};
+            let items = [aCompletedItem, unSelectedItem, expectedSelectedItem];
 
-            let goodList = [
-                {id: 'test', title: 'test', status: 'completed'},
-                {
-                    id: 'test2',
-                    title: 'test2',
-                    status: 'test2'
-                }
-            ];
-            // act
             // @ts-ignore to prove appropriate handling
-            component.items = goodList;
-            // assert
-            // @ts-ignore
-            expect(component.greatestCompletedItemIndex).toEqual(0);
-        });
-
-        it('should set select a progress item when when given a list of at least one progress item', () => {
-            // arrange
-            const aCompletedItem = {id: 'test', title: 'test', status: 'completed'};
-            let unSelectedItemToBeSelected = {id: 'test2', title: 'test2', status: 'test2'};
-            let expectedSelectedItem = {...unSelectedItemToBeSelected, focused: true};
-            let goodList = [aCompletedItem, unSelectedItemToBeSelected];
-            let listWithItemSelected = [aCompletedItem, expectedSelectedItem];
-
-            // act
-            component.items = goodList;
+            component.items = items;
 
             // assert
-            expect(component.currentSelectedItem).toEqual(expectedSelectedItem);
-            expect(component.items).toEqual(listWithItemSelected);
+            expect(component.currentSelectedItem.id).toEqual(expectedSelectedItem.id);
         });
     });
 
-    describe('findGreatestCompletedIndexFromItems', () => {
-        it('should return the correct item when given list of progress items with at least one completed item', () => {
+    describe('findFocusedIndexFromItems', () => {
+        it('should return the correct item when given list of progress items with at least one focused item', () => {
             // arrange
             let foundIndex = null;
             let listWithCompletedItem = [
-                {id: 'test', title: 'test', status: 'completed'},
+                {id: 'test', title: 'test', status: ProgressItemStatus.COMPLETE, focused: true},
                 {id: 'test2', title: 'test2', status: 'test2'},
                 {id: 'test3', title: 'test3', status: 'test3'}
             ];
             // act
             // @ts-ignore to prove appropriate handling
-            foundIndex = component.findGreatestCompletedIndexFromItems(listWithCompletedItem);
+            foundIndex = component.findFocusedIndexFromItems(listWithCompletedItem);
             // assert
             // @ts-ignore
             expect(foundIndex).toEqual(0);
         });
 
-        it('should return -1 when provided a list with no completed items', () => {
+        it('should return -1 when provided a list with no focused items', () => {
             // arrange
             let foundIndex = null;
             let listWithCompletedItem = [
@@ -110,7 +90,7 @@ describe('ProgressBarComponent', () => {
             ];
             // act
             // @ts-ignore to prove appropriate handling
-            foundIndex = component.findGreatestCompletedIndexFromItems(listWithCompletedItem);
+            foundIndex = component.findFocusedIndexFromItems(listWithCompletedItem);
             // assert
             // @ts-ignore
             expect(foundIndex).toEqual(-1);
@@ -124,7 +104,7 @@ describe('ProgressBarComponent', () => {
             const correctItemToReturn = {id: 'test2', title: 'test2', status: 'test2'};
             let returnedItem = null;
             const listWithOneCompletedItem = [
-                {id: 'test', title: 'test', status: 'completed'},
+                {id: 'test', title: 'test', status: ProgressItemStatus.COMPLETE},
                 correctItemToReturn,
                 {id: 'test3', title: 'test3', status: 'test3'}
             ];
@@ -139,11 +119,11 @@ describe('ProgressBarComponent', () => {
         it('should return the correct item when last completed is last item', () => {
             // arrange
             const greatestCompletedItemIndex = 2;
-            const correctItemToReturn = {id: 'test3', title: 'test3', status: 'completed'};
+            const correctItemToReturn = {id: 'test3', title: 'test3', status: ProgressItemStatus.COMPLETE};
             let returnedItem = null;
             const listWithAllItemsCompleted = [
-                {id: 'test', title: 'test', status: 'completed'},
-                {id: 'test2', title: 'test2', status: 'completed'},
+                {id: 'test', title: 'test', status: ProgressItemStatus.COMPLETE},
+                {id: 'test2', title: 'test2', status: ProgressItemStatus.COMPLETE},
                 correctItemToReturn
             ];
             // act
