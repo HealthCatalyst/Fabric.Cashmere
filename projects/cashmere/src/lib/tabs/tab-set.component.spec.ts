761dcc27078455971f9b6d5a2d12e9108d76780e
PJ Heebner <philip.heebner@healthcatalyst.com>
2019-09-16 10:03:49 -0400

    feat(tab): tabs are now ng-content based
    Use hc-tab for its template instead of
    just to generate tabs inside hc-tab-set.
    Pass template from hc-tab to hc-tab-set
    as tab content.
    Subscribe to tab clicks from parent component.
    Use TabService to communicate direction
    from parent to child components.
    Create scss files for tab-set.component and
    tab.component.
    Remove _tab.scss file in sass folder.
    Add description to tabContent member for API documentation.
    Add tests pertaining to changes made.
    Other tests should be added.
    Added basic tests for TabComponent.
    
    BREAKING CHANGE:
    
    Overrides to hc-tab-width css class
    will no longer work.
    The style change applied with this
    class is now applied using :host
    to refer to the hc-tab component.

--- projects/cashmere/src/lib/tabs/tab-set.component.spec.ts
+++ projects/cashmere/src/lib/tabs/tab-set.component.spec.ts
@@ -1,28 +1,99 @@
-// import {TabComponent} from './tab.component';
-// import {TabSetComponent} from './tab-set.component';
-// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-// import {RouterTestingModule} from '@angular/router/testing';
-//
-// describe('TabSetComponent', () => {
-//     let component: TabSetComponent;
-//     let fixture: ComponentFixture<TabSetComponent>;
-//
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             imports: [RouterTestingModule],
-//             declarations: [TabSetComponent, TabComponent]
-//         }).compileComponents();
-//     }));
-//
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(TabSetComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
-//
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+import {TabComponent} from './tab.component';
+import {TabSetComponent, TabChangeEvent} from './tab-set.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-// TODO: this is a pointless unit test but because of an issue with tabset this needs to be commented out see issue #467
+const expectedDirection: string = 'horizontal';
+
+@Component({
+    template: `
+        <hc-tab-set direction="${expectedDirection}">
+            <hc-tab tabTitle="Tab 1">
+                <div>Content 1</div>
+            </hc-tab>
+            <hc-tab tabTitle="Tab 2">
+                <div>Content 2</div>
+            </hc-tab>
+            <hc-tab tabTitle="Tab 3">
+                <div>Content 3</div>
+            </hc-tab>
+        </hc-tab-set>
+    `
+})
+export class TestTabSetComponent {}
+
+describe('TabSetComponent', () => {
+
+    let component: TabSetComponent;
+    let fixture: ComponentFixture<TestTabSetComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            declarations: [TabSetComponent, TabComponent, TestTabSetComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestTabSetComponent);
+        component = <TabSetComponent>fixture.debugElement.query(By.directive(TabSetComponent)).componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should reference its children', () => {
+        expect(component._tabs.length).toBe(3);
+        component._tabs.forEach((t, i) => expect(t.tabTitle).toBe(`Tab ${i + 1}`));
+    });
+
+    describe('direction', () => {
+        it('should be set from input', () => {
+            expect(component.direction).toBe(expectedDirection);
+        });
+
+        it('should be set for all children from parent input', () => {
+            component._tabs.forEach(t => expect(t._direction).toBe(expectedDirection));
+        });
+
+        it('should throw for unsupported direction', () => {
+            const invalidDirection = 'North';
+            expect(() => component.direction = invalidDirection)
+                .toThrow(new Error(`Unsupported tab direction value: ${invalidDirection}`));
+        });
+    });
+
+    describe('tabClick', () => {
+        it('should set clicked tab to active and other tabs to inactive', () => {
+            component._tabs.forEach(t => t._active = true);
+            component._tabs.last.tabClick.emit();
+
+            expect(component._tabs.last._active).toBeTruthy();
+            component._tabs.toArray().slice(0, 2)
+                .forEach(t => expect(t._active).toBeFalsy());
+        });
+
+        it('should set content to clicked tab\'s content', () => {
+            component.tabContent = component._tabs.last.tabContent;
+            component._tabs.first.tabClick.emit();
+
+            expect(component.tabContent).toEqual(component._tabs.first.tabContent);
+        });
+
+        it('should trigger selectedTabChange event', (done: () => void) => {
+            const expectedIndex: number = 0;
+            const expectedTab: TabComponent = component._tabs.first;
+            component.selectedTabChange.subscribe((e: TabChangeEvent) => {
+                expect(e.index).toEqual(expectedIndex);
+                expect(e.tab).toEqual(expectedTab);
+                done();
+            });
+
+            expectedTab.tabClick.emit();
+        });
+    });
+});
