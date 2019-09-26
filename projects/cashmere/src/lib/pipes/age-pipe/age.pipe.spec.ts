3f60bc8d8d03e219d3ad94dab9d8e48fbafd0233
Tia Dillman <tia.dillman@healthcatalyst.com>
2019-09-26 13:25:53 -0600

    feat(age.pipe): code changes per code review
    aliased sugar, removed testbed, added tests per code review
    
    feature #1005

--- projects/cashmere/src/lib/pipes/age-pipe/age.pipe.spec.ts
+++ projects/cashmere/src/lib/pipes/age-pipe/age.pipe.spec.ts
@@ -1,22 +1,16 @@
 
-import {async, inject, TestBed} from '@angular/core/testing';
-
 import { AgePipe } from './age.pipe';
-import { Date } from 'sugar';
+import { Date as SugarDate } from 'sugar';
 
 describe('AgePipe', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            providers: [AgePipe]
-        }).compileComponents();
-    }));
+    let pipe: AgePipe = new AgePipe();
 
-    it('create an instance', inject([AgePipe], pipe => {
+    it('create an instance', () => {
         // assert
         expect(pipe).toBeTruthy();
-    }));
+    });
 
-    it('give the duration in mos if less than a year', inject([AgePipe], pipe => {
+    it('give the duration in mos if less than a year', () => {
         // arrange
         const testDate: string = adjustDate(0, -2, 0).toDateString();
 
@@ -25,9 +19,9 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('2 mos');
-    }));
+    });
 
-    it('give the duration in yr if equal to one year', inject([AgePipe], pipe => {
+    it('give the duration in yr if equal to one year', () => {
         // arrange
         const testDate: string = adjustDate(-1, 0, 0).toDateString();
 
@@ -36,9 +30,9 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('1 yr');
-    }));
+    });
 
-    it('give the duration in mos if less than a year and not quite the birthdate', inject([AgePipe], pipe => {
+    it('give the duration in mos if less than a year and not quite the birthdate', () => {
         // arrange
         const testDate: string = adjustDate(0, -6, 1).toDateString();
 
@@ -47,9 +41,9 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('5 mos');
-    }));
+    });
 
-    it('give the duration as 0 mos if less than a month', inject([AgePipe], pipe => {
+    it('give the duration as 0 mos if less than a month', () => {
         // arrange
         const testDate: string = adjustDate(0, 0, -3).toDateString();
 
@@ -58,9 +52,9 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('0 mos');
-    }));
+    });
 
-    it('give the duration as 1 mo if equal to one month', inject([AgePipe], pipe => {
+    it('give the duration as 1 mo if equal to one month', () => {
         // arrange
         const testDate: string = adjustDate(0, 1, 0).toDateString();
 
@@ -69,9 +63,9 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('0 mos');
-    }));
+    });
 
-    it('give the duration in years if more than a year', inject([AgePipe], pipe => {
+    it('give the duration in years if more than a year', () => {
         // arrange
         const testDate: string = adjustDate(-3, 0, 0).toDateString();
 
@@ -80,9 +74,9 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('3 yrs');
-    }));
+    });
 
-    it('give the duration in years if much more than a year and not quite the birthdate', inject([AgePipe], pipe => {
+    it('give the duration in years if much more than a year and not quite the birthdate', () => {
         // arrange
         const testDate: string = adjustDate(-49, 0, 1).toISOString();
 
@@ -91,15 +85,64 @@ describe('AgePipe', () => {
 
         // assert
         expect(result).toEqual('48 yrs');
-    }));
+    });
+
+    it('return valid duration if given a simple valid string date', () => {
+        // arrange
+        const testDate: Date = adjustDate(-2, 0, 0);
+        const testDateStr: string = `${testDate.getMonth()}/${testDate.getDate()}/${testDate.getFullYear()}`;
+
+        // act
+        const result: string = pipe.transform(testDateStr);
+
+        // assert
+        expect(result).toEqual('2 yrs');
+    });
+
+    it('return "" if undefined when given undefined', () => {
+        // arrange
+        const testDate: any = undefined;
+
+        // act
+        const result: string = pipe.transform(testDate);
+
+        // assert
+        expect(result).toEqual('');
+    });
+
+    it('return "Invalid Birth Date" when an unexpected date string is given', () => {
+        // arrange
+        const testDate: string = "Test";
+        const consoleSpy: jasmine.Spy = spyOn(console, "log");
+
+        // act
+        const result: string = pipe.transform(testDate);
+
+        // assert
+        expect(consoleSpy).toHaveBeenCalledWith('AgePipe Error: Invalid Birth Date');
+        expect(result).toEqual('');
+    });
+
+    it('return "Value must be of type Date or String" when given a number', () => {
+        // arrange
+        const testDate: any = 25;
+        const consoleSpy: jasmine.Spy = spyOn(console, "log");
+
+        // act
+        const result: string = pipe.transform(testDate);
+
+        // assert
+        expect(consoleSpy).toHaveBeenCalledWith('AgePipe Error: Value must be of type Date or string');
+        expect(result).toEqual('');
+    });
 });
 
 export function adjustDate(adjustYearBy: number, adjustMonthBy: number, adjustDayBy: number ): Date {
-    let adjustedDate: Date = Date.create("today");
+    let adjustedDate: Date = SugarDate.create("today");
 
-    adjustedDate = Date.addYears(adjustedDate, adjustYearBy);
-    adjustedDate = Date.addMonths(adjustedDate, adjustMonthBy);
-    adjustedDate = Date.addDays(adjustedDate, adjustDayBy);
+    adjustedDate = SugarDate.addYears(adjustedDate, adjustYearBy);
+    adjustedDate = SugarDate.addMonths(adjustedDate, adjustMonthBy);
+    adjustedDate = SugarDate.addDays(adjustedDate, adjustDayBy);
 
     return adjustedDate;
 }
--- projects/cashmere/src/lib/pipes/age-pipe/age.pipe.ts
+++ projects/cashmere/src/lib/pipes/age-pipe/age.pipe.ts
@@ -1,21 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Date } from 'sugar';
+import { Date as SugarDate } from 'sugar';
+import chalk from 'chalk';
 
 @Pipe({
   'name': 'age'
 })
 export class AgePipe implements PipeTransform {
 
-  public transform(value: string): string {
-    const birth: Date = Date.create(value);
+  public transform(value: any): string {
+    if (!value) {
+        return '';
+    } else if (!(value instanceof Date) && typeof value !== "string") {
+        console.log(chalk.red("AgePipe Error: Value must be of type Date or string"));
+        return '';
+    }
+
+    const birth: Date = SugarDate.create(value);
+
+    // checks for valid date
+    if (isNaN(birth.getTime())) {
+        console.log(chalk.red("AgePipe Error: Invalid Birth Date"));
+        return '';
+    }
 
     // if under 12 months then show in months
-    let ageInMonths: number = Date.monthsAgo(birth);
+    let ageInMonths: number = SugarDate.monthsAgo(birth);
     if (ageInMonths < 12) {
       return `${ageInMonths} ` + ((ageInMonths === 0 || ageInMonths > 1) ? 'mos' : 'mo');
     }
 
-    let ageInYears: number = Date.yearsAgo(birth);
+    let ageInYears: number = SugarDate.yearsAgo(birth);
 
     return `${ageInYears} ` + (ageInYears > 1 ? 'yrs' : 'yr');
   }
