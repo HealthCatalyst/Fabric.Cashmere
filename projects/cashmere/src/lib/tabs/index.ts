be579ef74d56bde3699a6c6961b6b376b0ae126c
PJ Heebner <philip.heebner@healthcatalyst.com>
2019-10-01 10:17:25 -0400

    feat(tab): export TabsService
    Need to export TabsService so users can reference.

--- projects/cashmere/src/lib/tabs/index.ts
+++ projects/cashmere/src/lib/tabs/index.ts
@@ -2,3 +2,4 @@ export {TabComponent} from './tab/tab.component';
 export {TabSetComponent, TabChangeEvent} from './tab-set/tab-set.component';
 export {HcTabTitleComponent} from './tab/tab-title.component';
 export {TabsModule} from './tabs.module';
+export {TabsService} from './tabs.service';
