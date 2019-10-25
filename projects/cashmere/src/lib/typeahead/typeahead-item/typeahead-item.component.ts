267a2eb904a99c8d9d4c0cde5002eaa272c80af4
Barry Cann <bacann@wcf.com>
2019-10-25 14:08:42 -0600

    feat(typeahead highlighting): added configurable highlighting

--- projects/cashmere/src/lib/typeahead/typeahead-item/typeahead-item.component.ts
+++ projects/cashmere/src/lib/typeahead/typeahead-item/typeahead-item.component.ts
@@ -1,21 +1,34 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, Host} from '@angular/core';
+import {TypeaheadComponent} from '../typeahead.component';
 
 @Component({
     selector: 'hc-typeahead-item',
     templateUrl: './typeahead-item.component.html',
-    styleUrls: ['./typeahead-item.component.scss']
+    styleUrls: ['./typeahead-item.component.scss'],
 })
 export class TypeaheadItemComponent {
+
     _highlighted = false;
 
     /** Value of the option; also will be the value of the event that is emitted when an option is selected */
     @Input()
     value: any;
 
+    @Input()
+    typedChars: string;
+
     @Output()
     _selected: EventEmitter<any> = new EventEmitter<any>();
 
+    // Constructor(@Host() parent: TypeaheadComponent) {
+    //     console.log('this is parent: ', parent);
+    // }
+
     _itemSelected(item: any) {
         this._selected.emit(item);
+    }
+
+    itemHighlight(typedChars: string) {
+        this.typedChars = typedChars;
     }
 }
--- projects/cashmere/src/lib/typeahead/typeahead.module.ts
+++ projects/cashmere/src/lib/typeahead/typeahead.module.ts
@@ -4,11 +4,12 @@ import {TypeaheadItemComponent} from './typeahead-item/typeahead-item.component'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FormFieldModule} from '../form-field/hc-form-field.module';
 import {IconModule} from '../icon/icon.module';
+import {PipesModule} from '../pipes/pipes.module';
 import {CommonModule} from '@angular/common';
 import {InputModule} from '../input/input.module';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, FormFieldModule, IconModule, InputModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, FormFieldModule, IconModule, InputModule, PipesModule],
     exports: [TypeaheadComponent, TypeaheadItemComponent],
     declarations: [TypeaheadComponent, TypeaheadItemComponent]
 })
