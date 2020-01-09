62cb357005151aecfd6898dffc28d56ab53a456f
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2020-01-09 16:59:04 -0700

    feat(hc-menu): keyboard navigation
    allows users to navigate hcMenus using arrows or tab keys
    
    closes #1049

--- projects/cashmere/src/lib/pop/directives/menu-item.directive.ts
+++ projects/cashmere/src/lib/pop/directives/menu-item.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding} from '@angular/core';
+import {Directive, HostBinding, ElementRef, HostListener} from '@angular/core';
 
 /** Use `hcMenuItem` for a selectable item in an hcMenu. */
 @Directive({
@@ -7,4 +7,19 @@ import {Directive, HostBinding} from '@angular/core';
 export class MenuItemDirective {
     @HostBinding('class.hc-menu-item')
     _hostClass = true;
+
+    // Menu Item uses focus for hover highlighting to sync with keyboard navigation of the menu
+    @HostListener('mouseenter')
+    focus(): void {
+        this.ref.nativeElement.focus();
+    }
+
+    @HostListener('touchend')
+    @HostListener('touchcancel')
+    @HostListener('mouseleave')
+    blur(): void {
+        this.ref.nativeElement.blur();
+    }
+
+    constructor(public ref: ElementRef) {}
 }
