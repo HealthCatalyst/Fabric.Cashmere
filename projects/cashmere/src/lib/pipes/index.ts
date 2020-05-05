e724206ed613f9d168eabf7b98c24bbade5a8ee6
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2020-05-05 13:09:47 -0600

    feat(null-or-empty-pipe): adds null or empty pipe
    returns alternative string on null or empty

--- projects/cashmere/src/lib/pipes/index.ts
+++ projects/cashmere/src/lib/pipes/index.ts
@@ -1,3 +1,6 @@
+
+export {NullOrEmptyStringPipe} from './null-or-empty-string/null-or-empty-string.pipe';
+export {NullOrEmptyStringPipeModule} from './null-or-empty-string/null-or-empty-string-pipe.module';
 export {EllipsisPipe} from './ellipsis/ellipsis.pipe';
 export {EllipsisPipeModule} from './ellipsis/ellipsis-pipe.module';
 export {PipesModule} from './pipes.module';
--- projects/cashmere/src/lib/pipes/pipes.module.ts
+++ projects/cashmere/src/lib/pipes/pipes.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
-
 import {EllipsisPipeModule} from './ellipsis/ellipsis-pipe.module';
+import {NullOrEmptyStringPipeModule} from './null-or-empty-string/null-or-empty-string-pipe.module';
 
 @NgModule({
-    imports: [EllipsisPipeModule],
-    exports: [EllipsisPipeModule]
+    imports: [
+        EllipsisPipeModule,
+        NullOrEmptyStringPipeModule
+    ],
+    exports: [
+        EllipsisPipeModule,
+        NullOrEmptyStringPipeModule
+    ]
 })
 export class PipesModule {}
