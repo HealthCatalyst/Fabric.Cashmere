47f57da59b92ce4ec5a34e1cd226558223f83d32
Cfaust <cfaust@student.neumont.edu>
2020-07-01 15:39:36 -0600

    refactor(ellipsis-example): change template driven to reactive
    re #1205

--- projects/cashmere-examples/src/lib/ellipsis-overview/ellipsis-overview-example.component.ts
+++ projects/cashmere-examples/src/lib/ellipsis-overview/ellipsis-overview-example.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 /**
  * @title Ellipsis Pipe Overview
@@ -8,9 +9,10 @@ import {Component} from '@angular/core';
     templateUrl: 'ellipsis-overview-example.component.html'
 })
 export class EllipsisOverviewExampleComponent {
-    value: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Pellentesque iaculis ac ante eget sodales. Nulla posuere augue risus, ut
-    vestibulum nisi cursus eu. Sed in.`;
-    length: number = 15;
+    readonly value = new FormControl(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    // Pellentesque iaculis ac ante eget sodales. Nulla posuere augue risus, ut
+    // vestibulum nisi cursus eu. Sed in.`);
+    readonly length = new FormControl(15);
     mode: 'characters' | 'words' = 'characters';
+    readonly modeControl = new FormControl(this.mode);
 }
