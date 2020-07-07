fddffd5451f2d1bc5fd48dcb7173ec1ff82ae1cb
Cfaust <cfaust@student.neumont.edu>
2020-07-07 15:46:20 -0600

    fix(progress-dots): removed comments

--- projects/cashmere-examples/src/lib/progress-dots/progress-dots-example.component.ts
+++ projects/cashmere-examples/src/lib/progress-dots/progress-dots-example.component.ts
@@ -13,7 +13,4 @@ export class ProgressDotsExampleComponent {
     readonly dotsColor = new FormControl('light');
     readonly dotsMini = new FormControl(false);
     readonly dotsCentered = new FormControl(true);
-    // dotsColor = 'dark';
-    // dotsMini = false;
-    // dotsCentered = true;
 }
