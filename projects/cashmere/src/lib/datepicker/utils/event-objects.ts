6b6e83b238a130c589e8bcb6633ccf9abdf3044b
Cory Fugate <cory.fugate@healthcatalyst.com>
2020-07-17 15:39:26 -0600

    refactor(ivy compatibility): minor changes to prepare for ivy

--- projects/cashmere/src/lib/datepicker/utils/event-objects.ts
+++ projects/cashmere/src/lib/datepicker/utils/event-objects.ts
@@ -39,10 +39,8 @@ export function createMouseEvent(type: string, x = 0, y = 0, button = 0) {
 export function createTouchEvent(type: string, pageX = 0, pageY = 0) {
     // In favor of creating events that work for most of the browsers, the event is created
     // as a basic UI Event. The necessary details for the event will be set manually.
-    const event = document.createEvent('UIEvent');
+    const event = new UIEvent(type, { "bubbles": true, "cancelable": true, "detail": 0, "view": window} );
     const touchDetails = {pageX, pageY};
-
-    event.initUIEvent(type, true, true, window, 0);
 
     // Most of the browsers don't have a "initTouchEvent" method that can be used to define
     // the touch details.
