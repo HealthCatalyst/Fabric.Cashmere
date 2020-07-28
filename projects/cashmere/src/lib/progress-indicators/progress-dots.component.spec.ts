bf15439ae13949e57f1cbe924b42c71dbe09d9ff
Cfaust <cfaust@student.neumont.edu>
2020-07-28 15:16:50 -0600

    test(progress-dots): added missing unit test
    re #620

--- projects/cashmere/src/lib/progress-indicators/progress-dots.component.spec.ts
+++ projects/cashmere/src/lib/progress-indicators/progress-dots.component.spec.ts
@@ -2,22 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProgressDotsComponent } from './progress-dots.component';
 import { By } from '@angular/platform-browser';
-import { Component } from '@angular/core';
-
-// @Component({
-//     template: `
-//     <div class="hc-dots-container" [ngClass]="{'center-dots': isCentered }">
-//         <div class="hc-dots-loader" [ngClass]="{'loader-light': color === 'light', 'loader-mini': isMini}">
-//             <div class="loader-animate"></div>
-//         </div>
-//     </div>
-// `
-// })
-// export class ProgressDotsComponent {
-//     color: string = 'light';
-//     isCentered: boolean = false;
-//     isMini: boolean = false;
-// }
 
 describe('ProgressDotsComponent', () => {
     let component: ProgressDotsComponent;
@@ -51,19 +35,6 @@ describe('ProgressDotsComponent', () => {
                 fixture.detectChanges();
             }
             ).toThrow(new Error('Unsupported progress dots color value: beige'));
-        });
-    });
-    // TODO:
-    // Remove the F
-    fdescribe('d', () => {
-        it('should set the current step based on the activeIndex', () => {
-            console.log(component);
-            component.isCentered = false;
-            fixture.detectChanges();
-
-            let dotsContainer = fixture.debugElement.queryAll(By.css('hc-dots-container'));
-            console.log(dotsContainer);
-            // expect(dotsContainer[0].nativeElement.classList.contains('center-dots')).toBe(false);
         });
     });
 });
