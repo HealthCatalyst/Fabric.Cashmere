f95c9ef79d4fbad1298d1de8f3af41f6c96647db
Cory Fugate <cory.fugate@healthcatalyst.com>
2020-08-24 15:43:18 -0600

    feat(hc-select): adds support for compareWith
    also fixes binding issues in IE
    
    re #1352

--- projects/cashmere/src/lib/select/select.module.ts
+++ projects/cashmere/src/lib/select/select.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 import {SelectComponent} from './select.component';
 import {HcOptionDirective} from './hc-option.directive';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     exports: [SelectComponent, HcOptionDirective],
     declarations: [SelectComponent, HcOptionDirective]
 })
