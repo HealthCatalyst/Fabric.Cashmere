a170e08c3d429e9da35dccb10514d465952cf9cc
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2020-09-29 15:35:58 -0600

    feat(navbar): navbar co-branding
    adds the ability to add a logo to the right side of the navbar
    
    closes #875

--- projects/cashmere-examples/src/lib/navbar-app-switcher/navbar-app-switcher-example.component.ts
+++ projects/cashmere-examples/src/lib/navbar-app-switcher/navbar-app-switcher-example.component.ts
@@ -1,18 +1,27 @@
 import {Component} from '@angular/core';
-import {HcIcon} from '@healthcatalyst/cashmere';
+import {FormControl} from '@angular/forms';
+import {HcIcon, SelectChangeEvent} from '@healthcatalyst/cashmere';
 
 /**
  * @title Navbar app switcher with mobile menu example
  */
 @Component({
     selector: 'hc-navbar-app-switcher-example',
-    templateUrl: 'navbar-app-switcher-example.component.html'
+    templateUrl: 'navbar-app-switcher-example.component.html',
+    styleUrls: ['navbar-app-switcher-example.component.scss']
 })
 export class NavbarAppSwitcherExampleComponent {
     username = 'Christine K.';
     navIcon: HcIcon = {fontSet: "hc-icons", fontIcon: "hci-catalyst-logo", fontSize: 37};
+    brandingVal = 'light';
+    brandImg = './assets/MillrockLogo_light.png';
 
     // These values should match what was registered with the discovery service. We're using Atlas as an example here.
     currentAppName = 'Atlas4';
     currentAppVersion = '1';
+
+    brandChange( brandStyle: SelectChangeEvent ) {
+        this.brandingVal = brandStyle.value;
+        this.brandImg = './assets/MillrockLogo_' + brandStyle.value + '.png';
+    }
 }
---
+++ projects/cashmere/src/lib/navbar/navbar-brand/navbar-brand.component.ts
@@ -0,0 +1,43 @@
+import { Component, ViewEncapsulation, Input, HostBinding } from '@angular/core';
+
+/** Element to add a co-branding image to the right of the navbar */
+@Component({
+    selector: 'hc-navbar-brand',
+    template: `<img [src]="src" [ngStyle]="{height:_imgHeight, 'width':_imgWidth}" (load)="_checkRatio($event)">`,
+    styleUrls: ['./navbar-brand.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class NavbarBrandComponent {
+    _color: 'light' | 'dark' = 'light';
+    _imgHeight = '37px';
+    _imgWidth = 'auto';
+
+    @HostBinding('class') _hostClass = 'hc-navbar-brand-' + this.color;
+
+    /** Defines the path for the co-branding image.
+     * The image should be in landscape (horizontal) format; it will automatically be scaled to fit the navbar. */
+    @Input()
+    src: string;
+
+    /** Sets the background for co-branding; use `light` for images that require a white background. Defaults to `light` */
+    @Input()
+    get color(): 'light' | 'dark' {
+        return this._color;
+    }
+    set color( val: 'light' | 'dark' ) {
+        this._color = val;
+        this._hostClass = 'hc-navbar-brand-' + val;
+    }
+
+    /** Validate that the image will not be wider than 200px when scaled to 37px height; if not, scale the width rather than the height */
+    _checkRatio(evt) {
+        const width = evt.srcElement.width;
+        const height = evt.srcElement.height;
+        const ratio = width / height;
+
+        if ( ratio * 37 > 200 ) {
+            this._imgWidth = '200px';
+            this._imgHeight = 'auto';
+        }
+    }
+}
