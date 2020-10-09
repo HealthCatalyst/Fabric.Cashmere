1817603ae60fd055fa6c30e0d65ab9fe0035ccd8
Joe Skeen <joe.skeen@healthcatalyst.com>
2020-10-09 11:20:14 -0600

    fix(stackblitz): fix Bit imports
    include source of Bit components in the StackBlitz since @bit packages don't work
    
    fix #1395

---
+++ scripts/ensure-staged.ts
@@ -0,0 +1,9 @@
+import {execSync} from 'child_process';
+
+const stagedFiles = execSync('git diff --name-only --cached')
+    .toString()
+    .split(/\r?\n/g);
+
+if (stagedFiles.length < 2) {
+    throw new Error(`You can't commit until after you have staged some files!`);
+}
