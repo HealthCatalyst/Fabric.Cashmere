f65336f3f192f32038f11bbb7a5d2e637fa3f94b
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2020-11-14 12:42:53 -0700

    docs(highlighting): fixes syntax highlighting

---
+++ src/app/shared/highlight/highlight.directive.ts
@@ -0,0 +1,30 @@
+import {AfterViewInit, Directive, ElementRef, Input} from '@angular/core';
+import {highlightBlock} from 'highlight.js';
+
+@Directive({
+    selector: '[hcHighlight]'
+})
+export class HighlightDirective implements AfterViewInit {
+    @Input()
+    lineNumbers: boolean = true;
+
+    constructor(private el: ElementRef) {}
+
+    ngAfterViewInit() {
+        highlightBlock(this.el.nativeElement);
+        if (this.lineNumbers) {
+            this.addLines(this.el.nativeElement);
+        }
+    }
+
+    private addLines(pre: HTMLPreElement): void {
+        pre.innerHTML = `<span class="line-number"></span>${pre.innerHTML}<span class="cl"></span>`;
+        const num = pre.innerHTML.split(/\n/).length;
+        if (num > 2) {
+            for (let j = 1; j < num; j++) {
+                const lineNum = pre.getElementsByTagName('span')[0];
+                lineNum.innerHTML += `<span>${j}</span>`;
+            }
+        }
+    }
+}
