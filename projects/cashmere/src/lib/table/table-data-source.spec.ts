56895898f57c1fe4d09effb58abe699ddf242572
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2021-04-05 17:54:23 -0600

    test(table): refactor filter predicate tests

--- projects/cashmere/src/lib/table/table-data-source.spec.ts
+++ projects/cashmere/src/lib/table/table-data-source.spec.ts
@@ -141,37 +141,37 @@ describe('HcTableDataSource', () => {
                 ].toString()
             );
         });
+    });
 
-        describe('filterPredicate()', () => {
-            it('filters through the table by its id', () => {
-                let filter = "1";
-                const result = dataSource.filterPredicate(ELEMENT_DATA[0], filter);
-                expect(result).toBe(true);
-            });
+    describe('filterPredicate()', () => {
+        it('filters through the table by its id', () => {
+            let filter = "1";
+            const result = dataSource.filterPredicate({...ELEMENT_DATA[0]}, filter);
+            expect(result).toBe(true);
+        });
 
-            it('filters through the table by its name', () => {
-                let filter = "Hydrogen";
-                const result = dataSource.filterPredicate(ELEMENT_DATA[0], filter);
-                expect(result).toBe(true);
-            });
+        it('filters through the table by its name', () => {
+            let filter = "Hydrogen";
+            const result = dataSource.filterPredicate({...ELEMENT_DATA[0]}, filter);
+            expect(result).toBe(true);
+        });
 
-            it('filters through the table by its weight', () => {
-                let filter = "12.0107";
-                const result = dataSource.filterPredicate(ELEMENT_DATA[5], filter);
-                expect(result).toBe(true);
-            });
+        it('filters through the table by its weight', () => {
+            let filter = "12.0107";
+            const result = dataSource.filterPredicate({...ELEMENT_DATA[5]}, filter);
+            expect(result).toBe(true);
+        });
 
-            it('filters through the table by its id and name', () => {
-                let filter = "3 Lithium";
-                const result = dataSource.filterPredicate(ELEMENT_DATA[2], filter);
-                expect(result).toBe(true);
-            });
+        it('filters through the table by its id and name', () => {
+            let filter = "3 Lithium";
+            const result = dataSource.filterPredicate({...ELEMENT_DATA[2]}, filter);
+            expect(result).toBe(true);
+        });
 
-            it('filters through the table by its name and weight', () => {
-                let filter = "Carbon 12.0107";
-                const result = dataSource.filterPredicate(ELEMENT_DATA[5], filter);
-                expect(result).toBe(true);
-            });
+        it('filters through the table by its name and weight', () => {
+            let filter = "Carbon 12.0107";
+            const result = dataSource.filterPredicate({...ELEMENT_DATA[5]}, filter);
+            expect(result).toBe(true);
         });
     });
 });
