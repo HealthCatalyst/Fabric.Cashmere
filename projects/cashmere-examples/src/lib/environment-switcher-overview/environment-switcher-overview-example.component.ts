534e23fab0e9707e58016953e442577788eed959
Cory Fugate <cory.fugate@healthcatalyst.com>
2021-09-27 18:58:57 -0600

    refactor(env switcher): simplify interface

---
+++ projects/cashmere-examples/src/lib/environment-switcher-overview/environment-switcher-overview-example.component.ts
@@ -0,0 +1,59 @@
+import {HcIcon, IMetadataEnvironment} from '@healthcatalyst/cashmere';
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'hc-environment-switcher-overview-example',
+    templateUrl: 'environment-switcher-overview-example.component.html',
+    styleUrls: ['environment-switcher-overview-example.component.scss']
+})
+export class EnvironmentSwitcherOverviewExampleComponent {
+    username = 'Christine K.';
+    navIcon: HcIcon = {fontSet: 'hc-icons', fontIcon: 'hci-catalyst-logo', fontSize: 37};
+
+    selectedEnvironments = [1234];
+    environmentOptions: Array<IMetadataEnvironment> = [
+        {
+            id: 1234,
+            tenantCode: 'HCAT',
+            name: 'Production',
+            shortName: 'PROD',
+            description: 'Live customer environment.',
+            color: '#FFFFFF'
+        },
+        {
+            id: 5678,
+            tenantCode: 'HCAT',
+            name: 'Development',
+            shortName: 'DEV',
+            description: 'Environment for building and testing new features.',
+            color: '#E7C447'
+        },
+        {
+            id: 9012,
+            tenantCode: 'HCAT',
+            name: 'Test',
+            shortName: 'TEST',
+            description: 'Environment for integration testing.',
+            color: '#F13C45'
+        }
+    ];
+
+    multipleSelectEnabled = true;
+    newTabFeatureEnabled = true;
+    envSwitcherDisabled = false;
+    loading = false;
+    loadFailed = false;
+    eventOutput: {eventName: string; data: unknown};
+
+    onUpdateEnvironments($event: Array<IMetadataEnvironment>): void {
+        this.eventOutput = {eventName: 'updateEnvironments', data: $event};
+    }
+
+    onOpenInNewTab($event: IMetadataEnvironment): void {
+        this.eventOutput = {eventName: 'openInNewTab', data: $event};
+    }
+
+    onReloadEnvironments(): void {
+        this.eventOutput = {eventName: 'reloadEnvironments', data: ''};
+    }
+}
--- projects/cashmere/src/lib/env-switcher/env-switcher.module.ts
+++ projects/cashmere/src/lib/env-switcher/env-switcher.module.ts
@@ -1,14 +1,11 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 
 import {EnvSwitcherComponent} from './env-switcher.component';
 import {PopModule} from '../pop/popover.module';
 import {ProgressIndicatorsModule} from '../progress-indicators/progress-indicators.module';
-import {EnvironmentSwitcherService} from './env-switcher.service';
-import {WorkTrackerService} from '../shared/work-tracker.service';
 import {PipesModule} from '../pipes/pipes.module';
-import {IEnvSwitcherConfig, ENV_SWITCHER_CONFIG, ENV_SWITCHER_SERVICE} from './env-switcher-interfaces';
 import {IconModule} from '../icon/icon.module';
 import {CheckboxModule} from '../checkbox/checkbox.module';
 import {ButtonModule} from '../button/button.module';
@@ -17,25 +14,6 @@ import {ButtonModule} from '../button/button.module';
     imports: [CommonModule, PopModule, HttpClientModule, PipesModule, IconModule, ProgressIndicatorsModule, CheckboxModule, ButtonModule],
     declarations: [EnvSwitcherComponent],
     exports: [EnvSwitcherComponent],
-    entryComponents: [EnvSwitcherComponent],
-    providers: [
-        {
-            provide: ENV_SWITCHER_SERVICE,
-            useClass: EnvironmentSwitcherService
-        },
-        WorkTrackerService
-    ]
+    entryComponents: [EnvSwitcherComponent]
 })
-export class EnvSwitcherModule {
-    static forRoot(config: IEnvSwitcherConfig): ModuleWithProviders<EnvSwitcherModule> {
-        return {
-            ngModule: EnvSwitcherModule,
-            providers: [
-                {
-                    provide: ENV_SWITCHER_CONFIG,
-                    useValue: config
-                }
-            ]
-        };
-    }
-}
+export class EnvSwitcherModule {}
