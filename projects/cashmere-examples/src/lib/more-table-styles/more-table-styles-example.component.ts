550c5f66726231319bb64d7ab4a1fc36e4739174
Cory Fugate <cory.fugate@healthcatalyst.com>
2022-01-21 15:17:39 -0700

    feat(table styles): added styles for table actions and empty states
    re #1720

---
+++ projects/cashmere-examples/src/lib/more-table-styles/more-table-styles-example.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'hc-more-table-styles-example',
+    templateUrl: 'more-table-styles-example.component.html'
+})
+export class MoreTableStylesExampleComponent {}
