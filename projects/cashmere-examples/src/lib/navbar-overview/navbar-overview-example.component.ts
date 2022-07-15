d243f43f1fac0ff295745443db7b721f0cb8f279
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2022-07-15 16:00:54 -0600

    fix(navbar): component load more menu calc fix

--- projects/cashmere-examples/src/lib/navbar-overview/navbar-overview-example.component.ts
+++ projects/cashmere-examples/src/lib/navbar-overview/navbar-overview-example.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NavbarComponent } from '@healthcatalyst/cashmere';
 
 /**
  * @title Navbar overview
@@ -7,7 +8,19 @@ import {Component} from '@angular/core';
     selector: 'hc-navbar-overview-example',
     templateUrl: 'navbar-overview-example.component.html'
 })
-export class NavbarOverviewExampleComponent {
+export class NavbarOverviewExampleComponent implements AfterViewInit {
     username = 'Christine K.';
     organization = 'Millrock Hospital';
+
+    @ViewChild('exampleNavbar')
+    exampleNavbar: NavbarComponent;
+
+    ngAfterViewInit(): void {
+        /**  This call usually isn't necessary if your navbar is visible on page load.
+         * It's useful if your navbar appears after app load (like in this example)
+         * to ensure that the widths are correctly calculated for collapsing items into the More menu. */
+        setTimeout(() => {
+            this.exampleNavbar.refreshNavLinks();
+        }, 5);
+    }
 }
