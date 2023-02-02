f4254a6cf8bfcd84601e4b933501a197aacec91b
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2023-02-02 15:18:03 -0700

    docs(analytics): reorder analytics sections

--- src/app/analytics/analytics-routes.module.ts
+++ src/app/analytics/analytics-routes.module.ts
@@ -10,6 +10,166 @@ const routes: Routes = [
         component: AnalyticsComponent,
         children: [
             {
+                path: 'powerbi-template',
+                component: AnalyticsTemplateComponent,
+                data: {
+                    title: 'Template',
+                    category: 'Power BI'
+                }
+            },
+            {
+                path: 'powerbi-foundations',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Foundations',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/foundations.md')
+                }
+            },
+            {
+                path: 'powerbi-navbar',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Navbar',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/navbar.md')
+                }
+            },
+            {
+                path: 'powerbi-about',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'About Page',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/about.md')
+                }
+            },
+            {
+                path: 'powerbi-filters',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Filters',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/filters.md')
+                }
+            },
+            {
+                path: 'powerbi-tabs',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Tabs',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/tabs.md')
+                }
+            },
+            {
+                path: 'powerbi-metrics',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'KPI Metrics',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/kpi-metrics.md')
+                }
+            },
+            {
+                path: 'powerbi-charts',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Charts',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/charts.md')
+                }
+            },
+            {
+                path: 'powerbi-tables',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Tables',
+                    category: 'Power BI',
+                    document: require('raw-loader!../../../guides/analytics/powerbi/tables.md')
+                }
+            },
+            {
+                path: 'tableau-template',
+                component: AnalyticsTemplateComponent,
+                data: {
+                    title: 'Template',
+                    category: 'Tableau'
+                }
+            },
+            {
+                path: 'tableau-foundations',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Foundations',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/foundations.md')
+                }
+            },
+            {
+                path: 'tableau-navbar',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Navbar',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/navbar.md')
+                }
+            },
+            {
+                path: 'tableau-about',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'About Page',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/about.md')
+                }
+            },
+            {
+                path: 'tableau-filters',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Filters',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/filters.md')
+                }
+            },
+            {
+                path: 'tableau-tabs',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Tabs',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/tabs.md')
+                }
+            },
+            {
+                path: 'tableau-metrics',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'KPI Metrics',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/kpi-metrics.md')
+                }
+            },
+            {
+                path: 'tableau-charts',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Charts',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/charts.md')
+                }
+            },
+            {
+                path: 'tableau-tables',
+                component: MarkdownContentComponent,
+                data: {
+                    title: 'Tables',
+                    category: 'Tableau',
+                    document: require('raw-loader!../../../guides/analytics/tableau/tables.md')
+                }
+            },
+            {
                 path: 'qlik-template',
                 component: AnalyticsTemplateComponent,
                 data: {
@@ -99,168 +259,8 @@ const routes: Routes = [
                 }
             },
             {
-                path: 'tableau-template',
-                component: AnalyticsTemplateComponent,
-                data: {
-                    title: 'Template',
-                    category: 'Tableau'
-                }
-            },
-            {
-                path: 'tableau-foundations',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Foundations',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/foundations.md')
-                }
-            },
-            {
-                path: 'tableau-navbar',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Navbar',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/navbar.md')
-                }
-            },
-            {
-                path: 'tableau-about',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'About Page',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/about.md')
-                }
-            },
-            {
-                path: 'tableau-filters',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Filters',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/filters.md')
-                }
-            },
-            {
-                path: 'tableau-tabs',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Tabs',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/tabs.md')
-                }
-            },
-            {
-                path: 'tableau-metrics',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'KPI Metrics',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/kpi-metrics.md')
-                }
-            },
-            {
-                path: 'tableau-charts',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Charts',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/charts.md')
-                }
-            },
-            {
-                path: 'tableau-tables',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Tables',
-                    category: 'Tableau',
-                    document: require('raw-loader!../../../guides/analytics/tableau/tables.md')
-                }
-            },
-            {
-                path: 'powerbi-template',
-                component: AnalyticsTemplateComponent,
-                data: {
-                    title: 'Template',
-                    category: 'Power BI'
-                }
-            },
-            {
-                path: 'powerbi-foundations',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Foundations',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/foundations.md')
-                }
-            },
-            {
-                path: 'powerbi-navbar',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Navbar',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/navbar.md')
-                }
-            },
-            {
-                path: 'powerbi-about',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'About Page',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/about.md')
-                }
-            },
-            {
-                path: 'powerbi-filters',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Filters',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/filters.md')
-                }
-            },
-            {
-                path: 'powerbi-tabs',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Tabs',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/tabs.md')
-                }
-            },
-            {
-                path: 'powerbi-metrics',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'KPI Metrics',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/kpi-metrics.md')
-                }
-            },
-            {
-                path: 'powerbi-charts',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Charts',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/charts.md')
-                }
-            },
-            {
-                path: 'powerbi-tables',
-                component: MarkdownContentComponent,
-                data: {
-                    title: 'Tables',
-                    category: 'Power BI',
-                    document: require('raw-loader!../../../guides/analytics/powerbi/tables.md')
-                }
-            },
-            {
                 path: '**',
-                redirectTo: 'qlik-template'
+                redirectTo: 'powerbi-template'
             }
         ]
     }
