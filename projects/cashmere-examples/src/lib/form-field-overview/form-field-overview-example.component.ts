92b4efcd7cbfff1ec632b8f21ab68ff18641ab15
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2023-02-23 16:24:25 -0700

    fix(checkbox): set touched on click
    closes #2055

--- projects/cashmere-examples/src/lib/form-field-overview/form-field-overview-example.component.ts
+++ projects/cashmere-examples/src/lib/form-field-overview/form-field-overview-example.component.ts
@@ -16,10 +16,7 @@ export class FormFieldOverviewExampleComponent {
         selectControl: new FormControl('daily'),
         inputControl: new FormControl(''),
         radioControl: new FormControl('SM'),
-        checkOneControl: new FormControl(false),
-        checkTwoControl: new FormControl(false),
-        checkThreeControl: new FormControl(false),
-        checkFourControl: new FormControl(false),
+        checkControl: new FormControl(false),
         slideControl: new FormControl(true)
     });
 
@@ -36,10 +33,7 @@ export class FormFieldOverviewExampleComponent {
     invalidForm(): void {
         this.exampleForm.controls['inputControl'].setErrors({incorrect: true});
         this.exampleForm.controls['selectControl'].setErrors({incorrect: true});
-        this.exampleForm.controls['checkOneControl'].setErrors({incorrect: true});
-        this.exampleForm.controls['checkTwoControl'].setErrors({incorrect: true});
-        this.exampleForm.controls['checkThreeControl'].setErrors({incorrect: true});
-        this.exampleForm.controls['checkFourControl'].setErrors({incorrect: true});
+        this.exampleForm.controls['checkControl'].setErrors({incorrect: true});
         this.exampleForm.controls['radioControl'].setErrors({incorrect: true});
         this.exampleForm.controls['slideControl'].setErrors({incorrect: true});
     }
