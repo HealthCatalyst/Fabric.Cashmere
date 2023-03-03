b4e125d5de7d32f04e6e5f340bfb6750fb926588
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2023-03-03 15:49:26 -0700

    build(angular): remove deprecated raw-loader calls
    closes #1718

--- src/app/guides/guides.service.ts
+++ src/app/guides/guides.service.ts
@@ -15,61 +15,61 @@ export class GuidesService {
             title: 'Getting Started',
             route: 'getting-started',
             category: 'using',
-            document: require('raw-loader!../../../guides/getting-started.md')
+            document: require('../../../guides/getting-started.md')
         },
         {
             title: 'Guidelines for Contribution',
             route: 'contribution-guide',
             category: 'dev',
-            document: require('raw-loader!../../../guides/contribution-guide.md')
+            document: require('../../../guides/contribution-guide.md')
         },
         {
             title: 'Naming Conventions',
             route: 'naming-conventions',
             category: 'dev',
-            document: require('raw-loader!../../../guides/naming-conventions.md')
+            document: require('../../../guides/naming-conventions.md')
         },
         {
             title: 'Submit an Issue',
             route: 'submit-an-issue',
             category: 'using',
-            document: require('raw-loader!../../../guides/submit-an-issue.md')
+            document: require('../../../guides/submit-an-issue.md')
         },
         {
             title: 'Supported Browsers',
             route: 'supported-browsers',
             category: 'using',
-            document: require('raw-loader!../../../guides/supported-browsers.md')
+            document: require('../../../guides/supported-browsers.md')
         },
         {
             title: 'Supported Angular Versions',
             route: 'supported-angular-versions',
             category: 'using',
-            document: require('raw-loader!../../../guides/supported-angular-versions.md')
+            document: require('../../../guides/supported-angular-versions.md')
         },
         {
             title: 'Creating New Components',
             route: 'creating-new-components',
             category: 'dev',
-            document: require('raw-loader!../../../guides/creating-new-components.md')
+            document: require('../../../guides/creating-new-components.md')
         },
         {
             title: 'Customizing Components',
             route: 'using-customizing-components',
             category: 'dev',
-            document: require('raw-loader!../../../guides/using-customizing-components.md')
+            document: require('../../../guides/using-customizing-components.md')
         },
         {
             title: 'Working with Examples',
             route: 'working-with-examples',
             category: 'dev',
-            document: require('raw-loader!../../../guides/working-with-examples.md')
+            document: require('../../../guides/working-with-examples.md')
         },
         {
             title: 'Linking a Local App to Cashmere',
             route: 'linking-a-local-app-to-cashmere',
             category: 'dev',
-            document: require('raw-loader!../../../guides/linking-a-local-app-to-cashmere.md')
+            document: require('../../../guides/linking-a-local-app-to-cashmere.md')
         }
     ];
 }
