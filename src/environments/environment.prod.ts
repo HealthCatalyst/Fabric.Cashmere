8aa66dc09e21aabd8a15ab277695535f07e4b7c7
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2023-09-18 16:10:03 -0600

    docs(terminology): migrate to Azure resources

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: true,
-    instrumentationKey: '7ce7e52a-addf-427b-91cf-094dd0ea1df0'
+    instrumentationKey: '7ce7e52a-addf-427b-91cf-094dd0ea1df0',
+    productCatalog: {
+        url: 'https://productdatabase-prod-svc.azurewebsites.net'
+    }
 };
