ef5d8038673011a723b2459ebec98f56dc710507
sivey <sivey@armus.com>
2024-02-16 12:26:01 -0800

    docs(app-branding-demo): adding output formats png/jpeg and fixing color

--- src/app/foundations/app-branding/app-branding-demo.component.ts
+++ src/app/foundations/app-branding/app-branding-demo.component.ts
@@ -14,34 +14,41 @@ export class AppBrandingDemoComponent extends BaseDemoComponent {
     theme = "dark";
     slimText = "Cashmere";
     boldText = "Dashboard";
+    outputFormat = 'image/png';
 
     constructor(sectionService: SectionService) {
         super(sectionService);
     }
 
     ngAfterViewInit() {
-        setTimeout(() => this.generateLogo(), 2000); // font may not have loaded yet
-        this.generateLogo();
+        setTimeout(() => this.generateLogo(), 2000); // font may not have loaded yet :(
+        setTimeout(() => this.generateLogo(), 500);
     }
 
     generateLogo() {
         const canvas = this.canvasContainer.nativeElement;
         const ctx = canvas.getContext("2d", {alpha: true});
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        let textColor = "";
+        let slimTextColor = "";
+        let boldTextColor = "";
         if(this.theme === "light") {
-            textColor = "#000000";
+            slimTextColor = "#0b0c0c"; // slightly grey
+            boldTextColor = "#000000";
             this.imageOutput.nativeElement.style.backgroundColor = "#ffffff";
         }
         else {
-            textColor = "#ffffff";
+            slimTextColor = "#f4f3f3"; // slightly grey
+            boldTextColor = "#ffffff";
             this.imageOutput.nativeElement.style.backgroundColor = "#384655";
         }
+
+
         const fontFace = "BrandFont";
         const slimTextFont = "182px " + fontFace;
         const boldTextFont = "bold 181px " + fontFace;
         const slimTextLetterSpacing = "-2.5px";
         const boldTextLetterSpacing = "-2.5px";
+
+        // calculate sizing and positioning of text.
         ctx.font = slimTextFont;
         ctx.letterSpacing = slimTextLetterSpacing;
         const slimTextRect = ctx.measureText(this.slimText);
@@ -50,24 +57,34 @@ export class AppBrandingDemoComponent extends BaseDemoComponent {
         const boldTextRect = ctx.measureText(this.boldText);
         canvas.width = slimTextRect.width + 2 + boldTextRect.width;
         canvas.height = 203;
-
         const textOffsetX = 0;
         const textOffsetY = 157; //slimTextRect.fontBoundingBoxAscent-21;
 
-        // slim text
+        // render background
+        if(this.outputFormat === "image/jpeg") { 
+            // solid background
+            ctx.fillStyle = this.imageOutput.nativeElement.style.backgroundColor;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+        else { 
+            // transparent background
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+
+        // render slim text
         ctx.font = slimTextFont;
         ctx.letterSpacing = slimTextLetterSpacing;
-        ctx.fillStyle = textColor; // setting font resets fillStyle :P
+        ctx.fillStyle = slimTextColor; // setting font resets fillStyle :P
         ctx.fillText(this.slimText, textOffsetX, textOffsetY);
 
-        //bold text
+        // render bold text
         ctx.font = boldTextFont;
         ctx.letterSpacing = boldTextLetterSpacing;
-        ctx.fillStyle = textColor; // setting font resets fillStyle :P
-        ctx.fillText(this.boldText, textOffsetX + slimTextRect.width - 6 , textOffsetY);
+        ctx.fillStyle = boldTextColor; // setting font resets fillStyle :P
+        ctx.fillText(this.boldText, textOffsetX + slimTextRect.width, textOffsetY);
 
         // create data url
-        const dataUrl = canvas.toDataURL();
+        const dataUrl = canvas.toDataURL(this.outputFormat);
         this.imageOutput.nativeElement.src = dataUrl;
 
         // create a clean download link
@@ -76,7 +93,8 @@ export class AppBrandingDemoComponent extends BaseDemoComponent {
             downloadLink.href = dataUrl;
             let filename = `${this.slimText}${this.boldText ? "-" + this.boldText : ""}`;
             filename = filename.toLowerCase().replace(/[^0-1a-z\s-]/, '').trim().replace(/\s/, '-');
-            downloadLink.download = filename + `.png`;
+            const fileFormat = this.outputFormat.split("/")[1];
+            downloadLink.download = `${filename}.${fileFormat}`;
         }
     }
 }
