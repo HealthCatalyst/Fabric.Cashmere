86f3b8d0208e9860cf4cd672ef6c6eccae5b3845
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2024-05-16 10:05:18 -0600

    feat(progress-indicators): progress bar component
    adds progress bar component to progress indicators module
    
    closes #2295

---
+++ projects/cashmere/src/lib/progress-indicators/progress-bar.component.spec.ts
@@ -0,0 +1,33 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProgressBarComponent } from './progress-bar.component';
+
+describe('ProgressBarComponent', () => {
+    let component: ProgressBarComponent;
+    let fixture: ComponentFixture<ProgressBarComponent>;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [ProgressBarComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProgressBarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('Color', () => {
+        it('should throw an error on bad color', () => {
+            expect(() => {
+                component.color = 'beige' as string;
+                fixture.detectChanges();
+            }
+            ).toThrow(new Error('Unsupported progress bar color value: beige'));
+        });
+    });
+});
