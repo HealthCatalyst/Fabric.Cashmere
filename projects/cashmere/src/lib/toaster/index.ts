c9c2a9e9356685cac7158c1c7a6b19bc945662d7
Cory Fugate <cory.fugate@healthcatalyst.com>
2026-02-06 14:42:05 -0700

    fix(types): explicit type exports
    adding exports for PopoverTypes and ToastContentType
    
    re #2453

--- projects/cashmere/src/lib/toaster/index.ts
+++ projects/cashmere/src/lib/toaster/index.ts
@@ -1,5 +1,5 @@
 export {HcToastComponent} from './hc-toast.component';
 export {HcToastOptions} from './hc-toast-options';
 export {HcToastRef} from './hc-toast-ref';
-export {HcToasterService} from './hc-toaster.service';
+export {HcToasterService, ToastContentType} from './hc-toaster.service';
 export {ToasterModule} from './hc-toaster.module';
